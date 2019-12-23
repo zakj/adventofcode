16dbd514d2cc8a68f1fa87ba97508ecab83f1c66
Zak Johnson <me@zakj.net>
2019-12-23 15:52:32 -0800

    Bugfix day 3

--- 2019/day03.ts
+++ 2019/day03.ts
@@ -29,9 +29,12 @@ const keyToPoint = (s: string): Point => {
 };
 
 const getRange = (start: number, end: number): number[] => {
-  return Array(Math.abs(end - start) + 1)
-    .fill(0)
-    .map((x, i) => i + Math.min(start, end));
+  const step = start < end ? 1 : -1;
+  const arr = [];
+  for (let i = start; step > 0 ? i <= end : i >= end; i += step) {
+    arr.push(i);
+  }
+  return arr;
 };
 const directions: {[key: string]: (x: number, y: number, d: number) => Point} = {
   L: (x, y, d) => [x - d, y],
@@ -74,7 +77,7 @@ function closestIntersectionByDistance(wires: Wire[]) {
 
 function closestIntersectionBySteps(wires: Wire[]) {
   const grid = defaultDict<number[]>(() => Array(wires.length).fill(0));
-  wires.forEach((wire, i) => {
+  wires.forEach((wire, wireIndex) => {
     let x = 0;
     let y = 0;
     let steps = 0;
@@ -83,7 +86,7 @@ function closestIntersectionBySteps(wires: Wire[]) {
       path.forEach(([x, y]) => {
         steps++;
         const gridSteps = grid[pointToKey(x, y)];
-        if (!gridSteps[i]) gridSteps[i] = steps;
+        if (!gridSteps[wireIndex]) gridSteps[wireIndex] = steps;
       })
       const q = directions[move.direction](x, y, move.distance);
       [x, y] = q; // TODO whyyyyy
@@ -92,32 +95,11 @@ function closestIntersectionBySteps(wires: Wire[]) {
 
   const totalSteps = Object.entries(grid)
     .filter(([point, steps]) => point !== '0,0' && steps.every(x => x > 0))
-    .map(([point, steps]) => [steps.reduce((acc, x) => acc + x, 0), keyToPoint(point)])
-    // .sort((a, b) => a[0] - b[0])
-    .sort((a, b) => {
-      console.log(a);
-      return -1;
-    });
+    .map(([point, steps]): [number, Point] => [steps.reduce((acc, x) => acc + x, 0), keyToPoint(point)])
+    .sort((a, b) => a[0] - b[0]);
 
   return totalSteps[0];
 }
-
-// const d1: Wire[] = `R75,D30,R83,U83,L12,D49,R71,U7,L72
-// U62,R66,U55,R34,D71,R55,D58,R83`.split('\n').map(wire => {
-//   return wire.split(',').map(move => ({
-//     direction: move[0] as Direction,
-//     distance: parseInt(move.slice(1), 10),
-//   }))
-// });
-// const d2: Wire[] = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-// U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`.split('\n').map(wire => {
-//   return wire.split(',').map(move => ({
-//     direction: move[0] as Direction,
-//     distance: parseInt(move.slice(1), 10),
-//   }))
-// });
-// console.info(610, closestIntersectionBySteps(d1));
-// console.info(410, closestIntersectionBySteps(d2));
 
 console.log(closestIntersectionByDistance(data));
 console.log(closestIntersectionBySteps(data));
