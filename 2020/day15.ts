13dda30607295dc2c67d8e31a698a3c1734685cd
Zak Johnson <me@zakj.net>
2020-12-14 23:10:37 -0800

    Add slow Day 15 :/

---
+++ 2020/day15.ts
@@ -0,0 +1,34 @@
+import { answers, example, loadDayLines } from './util';
+
+
+function spokenNumber(input: number[], n: number): number {
+  const lastSaid: {[k: number]: number[]} = {};
+  input.forEach((x, i) => {
+    lastSaid[x] ||= []
+    lastSaid[x].push(i)
+  })
+  let last = input[input.length - 1];
+  for (let i = input.length; i < n; ++i) {
+    let speak = 0;
+    if (lastSaid[last].length > 1) {
+      speak = lastSaid[last][lastSaid[last].length - 1] - lastSaid[last][lastSaid[last].length - 2];
+    }
+    lastSaid[speak] ||= [];
+    lastSaid[speak].push(i);
+    last = speak;
+  }
+  return last;
+}
+
+example.equal(0, spokenNumber([0, 3, 6], 10));
+example.equal(1, spokenNumber([1, 3, 2], 2020));
+example.equal(10, spokenNumber([2, 1, 3], 2020));
+example.equal(27, spokenNumber([1, 2, 3], 2020));
+example.equal(78, spokenNumber([2, 3, 1], 2020));
+example.equal(438, spokenNumber([3, 2, 1], 2020));
+example.equal(1836, spokenNumber([3, 1, 2], 2020));
+
+answers(
+  () => spokenNumber([18,8,0,5,4,1,20], 2020),
+  () => spokenNumber([18,8,0,5,4,1,20], 30000000),
+)
