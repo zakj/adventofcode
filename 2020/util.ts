f15f365ba2ed86c2615d405f22e7e4b7a21c4567
Zak Johnson <me@zakj.net>
2020-12-14 23:10:08 -0800

    Add perf and colors util

--- 2020/util.ts
+++ 2020/util.ts
@@ -1,6 +1,31 @@
 import { strict as assert } from 'assert';
 import * as fs from 'fs';
 import { resolve } from 'path';
+import { performance } from 'perf_hooks';
+
+export function answers(...fns: (() => any)[]): void {
+  const colors = {
+    green: 32,
+    grey: 90,
+  };
+  const color = (c, text) => `\x1b[${colors[c]}m${text}\x1b[89m`;
+
+  fns.forEach((fn, i) => {
+    const start = performance.now();
+    const result = fn();
+    const durationMs = performance.now() - start;
+    const duration =
+      durationMs > 1000
+        ? `${(durationMs / 1000).toFixed(3)}s`
+        : `${durationMs.toFixed(3)}m`;
+    const width = process.stdout.columns - duration.length - 6;
+    console.log(
+      `${i + 1}:`,
+      color('green', result.toString().padEnd(width)),
+      color('grey', `(${duration})`)
+    );
+  });
+}
 
 const assertHandler: ProxyHandler<typeof assert> = {
   get: function (target, prop, receiver) {
@@ -28,7 +53,7 @@ export function loadDayLines(n: number, prefix?: string): string[] {
   return loadDay(n, prefix).split('\n');
 }
 
-export type Maybe<T> = T | typeof Nothing
+export type Maybe<T> = T | typeof Nothing;
 export const Nothing = Symbol('Nothing');
 
 export const product = (xs: number[]): number => xs.reduce((acc, x) => acc * x);
