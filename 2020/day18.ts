c486bf97aae6f813c461f49b8fd3e3bfae6c3957
Zak Johnson <me@zakj.net>
2020-12-18 02:05:30 -0800

    Day 18 part 2

--- 2020/day18.ts
+++ 2020/day18.ts
@@ -3,10 +3,13 @@ import { answers, example, loadDayLines, sum } from './util';
 const isDigit = (c: string): boolean => /\d/.test(c);
 const isOperator = (c: string): boolean => ['+', '*'].includes(c);
 
-function evaluate(s: string): number {
+type Token = number | '*' | '+' | Token[];
+
+function tokenize(s: string, evaluate: (s: string) => number): Token[] {
   let buffer = '';
   let openParens = 0;
   let stack = [];
+
   [...s.replaceAll(/\s+/g, '')].forEach((c) => {
     if (isDigit(c)) {
       buffer += c;
@@ -37,14 +40,37 @@ function evaluate(s: string): number {
   });
   if (buffer) stack.push(Number(buffer));
 
-  let result = stack.shift();
+  return stack;
+}
+
+function evaluate(s: string): number {
+  let stack = tokenize(s, evaluate);
+  let result = stack.shift() as number;
   for (let i = 0; i < stack.length; i += 2) {
     const op = stack[i];
-    const val = stack[i + 1];
+    const val = stack[i + 1] as number;
     if (op === '+') result += val;
     if (op === '*') result *= val;
   }
 
+  return result;
+}
+
+function evaluatePrecedence(s: string): number {
+  let stack = tokenize(s, evaluatePrecedence);
+
+  let i;
+  while ((i = stack.findIndex(x => x === '+')) !== -1) {
+    const sum = (stack[i - 1] as number) + (stack[i + 1] as number);
+    stack = [].concat(stack.slice(0, i - 1), sum, stack.slice(i + 2))
+  }
+
+  let result = stack.shift() as number;
+  for (let i = 0; i < stack.length; i += 2) {
+    const op = stack[i];
+    const val = stack[i + 1] as number;
+    if (op === '*') result *= val;
+  }
   return result;
 }
 
@@ -56,5 +82,15 @@ example.equal(
   13632
 );
 
+example.equal(evaluatePrecedence('1 + (2 * 3) + (4 * (5 + 6))'), 51)
+example.equal(evaluatePrecedence('2 * 3 + (4 * 5)'), 46)
+example.equal(evaluatePrecedence('5 + (8 * 3 + 9 + 3 * 4 * 3)'), 1445)
+example.equal(evaluatePrecedence('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'), 669060)
+example.equal(evaluatePrecedence('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'), 23340)
+
+
 const expressions = loadDayLines(18);
-answers(() => sum(expressions.map(evaluate)));
+answers(
+  () => sum(expressions.map(evaluate)),
+  () => sum(expressions.map(evaluatePrecedence)),
+);
