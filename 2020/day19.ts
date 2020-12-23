65dbbc05435a0c943bbc7cea14c6b1c26960e6cd
Zak Johnson <me@zakj.net>
2020-12-22 18:33:51 -0800

    Day 19 part 2

--- 2020/day19.ts
+++ 2020/day19.ts
@@ -1,47 +1,52 @@
-import { answers, cartesianProduct, example, loadDay } from "./util";
+import { answers, example, loadDay } from './util';
 
-type Literal = string;
 type RuleId = number;
-type RuleSequence = (Literal | RuleId)[];
+type RuleSequence = (string | RuleId)[];
 type Rule = RuleSequence[];
-type Rules = Map<RuleId, Rule>
+type Rules = Map<RuleId, Rule>;
 
 function parseRules(s: string): Rules {
-  return new Map(s.split('\n').map(line => {
-    const [id, ruleStr] = line.split(': ');
-    const optionsStrs = ruleStr.split(' | ')
-    return [Number(id), optionsStrs.map(opt => opt.split(' ').map(x => {
-      return x.match(/^\d+$/) ? Number(x) : x.replaceAll(/"/g, '');
-    }))];
-  }));
-}
-
-function expandedRule(rules: Rules, id: number, cache: Map<RuleId, string[]>): string[] {
-  if (!cache.get(id)) {
-    const options = rules.get(id).map(seq => {
-      const seqOptions = seq.map(x => {
-        if (typeof x === 'number') return expandedRule(rules, x, cache)
-        else return [x];
-      })
-      return cartesianProduct(...seqOptions).map(x => x.join(''))
+  return new Map(
+    s.split('\n').map((line) => {
+      const [id, ruleStr] = line.split(': ');
+      const rule = ruleStr.split(' | ').map((opt) =>
+        opt.split(' ').map((x) => {
+          return x.match(/^\d+$/) ? Number(x) : x.replaceAll(/"/g, '');
+        })
+      );
+      return [Number(id), rule];
     })
-    cache.set(id, Array.from(new Set(cartesianProduct(...options).flat())))
-  }
-  return cache.get(id)
+  );
 }
 
-function part1Valid(rules: Rules, id: number, messages: string[]) {
-  const cache = new Map<RuleId, string[]>();
-  const expandedRules = new Set(expandedRule(rules, id, cache))
-  return messages.filter(m => expandedRules.has(m))
+function canStringMatchRules(s: string, rules: Rules, seq: RuleSequence): boolean {
+  if (!s.length || !seq.length) return s.length === seq.length;
+  const [head, ...tail] = seq;
+  if (typeof head === 'string') {
+    if (s[0] !== head) return false;
+    return canStringMatchRules(s.slice(1), rules, tail);
+  } else {
+    return rules.get(head).some((x) => canStringMatchRules(s, rules, [].concat(x, tail)));
+  }
 }
 
 const exampleInput = loadDay(19, 'example').split('\n\n');
 const exampleRules = parseRules(exampleInput[0]);
-const exampleMessages = exampleInput[1].split('\n')
-example.equal(2, part1Valid(exampleRules, 0, exampleMessages).length);
+const exampleMessages = exampleInput[1].split('\n');
+example.equal(
+  2,
+  exampleMessages.filter((m) => canStringMatchRules(m, exampleRules, [0]))
+    .length
+);
 
 const input = loadDay(19).split('\n\n');
-const rules = parseRules(input[0])
-const messages = input[1].split('\n')
-answers(() => part1Valid(rules, 0, messages).length)
+const rules = parseRules(input[0]);
+const messages = input[1].split('\n');
+answers(
+  () => messages.filter((m) => canStringMatchRules(m, rules, [0])).length,
+  () => {
+    rules.get(8).push([42, 8]);
+    rules.get(11).push([42, 11, 31]);
+    return messages.filter((m) => canStringMatchRules(m, rules, [0])).length;
+  }
+);
