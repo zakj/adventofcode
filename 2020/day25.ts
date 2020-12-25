bad8893083142993e9bef13714b00a2e7be7f4ff
Zak Johnson <me@zakj.net>
2020-12-24 22:03:42 -0800

    Day 25 part 1

---
+++ 2020/day25.ts
@@ -0,0 +1,46 @@
+import { answers, example } from './util';
+
+const INITIAL_SUBJECT = 7;
+
+function* transformSubjectSearch(subject: number): Generator<[number, number]> {
+  let loopSize = 0;
+  let value = 1;
+  while (true) {
+    loopSize += 1;
+    value *= subject;
+    value %= 20201227;
+    yield [loopSize, value];
+  }
+}
+
+function transformSubject(subject: number, loopSize: number): number {
+  let value = 1;
+  for (let i = 1; i <= loopSize; ++i) {
+    value *= subject;
+    value %= 20201227;
+  }
+  return value;
+}
+
+function findLoopSize(pubKey: number): number {
+  for (let [loopSize, value] of transformSubjectSearch(INITIAL_SUBJECT)) {
+    if (value === pubKey) return loopSize;
+  }
+}
+
+function encryptionKey(pubKeys: number[]): number {
+  const loopSizes = pubKeys.map(findLoopSize);
+  return transformSubject(pubKeys[0], loopSizes[1]);
+}
+
+const examplePubKeys = [5764801, 17807724];
+example.equal(8, findLoopSize(examplePubKeys[0]));
+example.equal(11, findLoopSize(examplePubKeys[1]));
+example.equal(examplePubKeys[0], transformSubject(7, 8));
+example.equal(examplePubKeys[1], transformSubject(7, 11));
+example.equal(14897079, encryptionKey(examplePubKeys));
+
+const pubKeys = [10604480, 4126658];
+answers(
+  () => encryptionKey(pubKeys)
+)
