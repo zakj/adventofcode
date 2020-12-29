1f6acaf4929b37381dd1232e8ec1374399ca1056
Zak Johnson <me@zakj.net>
2020-12-28 23:52:52 -0800

    Day 21 minor cleanup

--- 2020/day21.ts
+++ 2020/day21.ts
@@ -1,9 +1,9 @@
 import { answers, example, loadDayLines } from './util';
 
 type Food = {
-  ingredients: Set<string>
-  allergens: Set<string>
-}
+  ingredients: Set<string>;
+  allergens: Set<string>;
+};
 
 function parseFoods(lines: string[]): Food[] {
   return lines.map((line) => {
@@ -15,31 +15,34 @@ function parseFoods(lines: string[]): Food[] {
   });
 }
 
-function intersectAll<T>(sets: Set<T>[]): Set<T> {
-  const [head, ...tail] = sets;
-  return new Set([...head].filter(x => tail.every(xs => xs.has(x))))
+function intersect<T>(a: Set<T>, b: Set<T>): Set<T> {
+  return new Set([...a].filter((x) => b.has(x)));
+}
+
+function difference<T>(a: Set<T>, b: Set<T>): Set<T> {
+  return new Set([...a].filter((x) => !b.has(x)));
 }
 
 function findAllergens(foods: Food[]): Map<string, string> {
-  const possibles: Record<string, Set<string>[]> = {};
-  for (const food of foods) {
+  const possibles = foods.reduce((acc, food) => {
     for (const allergen of food.allergens) {
-      possibles[allergen] ||= [];
-      possibles[allergen].push(new Set(food.ingredients))
+      acc.set(
+        allergen,
+        acc.has(allergen)
+          ? intersect(acc.get(allergen), food.ingredients)
+          : food.ingredients
+      );
     }
-  }
+    return acc;
+  }, new Map<string, Set<string>>());
 
-  const allergens = new Map();
-  while (allergens.size < Object.values(possibles).length) {
-    for (const [allergen, options] of Object.entries(possibles)) {
-      options.forEach(o => {
-        for (const a of allergens.values()) {
-          o.delete(a)
-        }
-      })
-      const intr = intersectAll(options);
-      if (intr.size === 1) allergens.set(allergen, [...intr][0]);
-      possibles[allergen] = [intr];
+  const allergens = new Map<string, string>();
+  while (allergens.size < possibles.size) {
+    for (const [allergen, ingr] of possibles.entries()) {
+      for (const a of allergens.values()) {
+        ingr.delete(a);
+      }
+      if (ingr.size === 1) allergens.set(allergen, [...ingr][0]);
     }
   }
   return allergens;
@@ -47,17 +50,15 @@ function findAllergens(foods: Food[]): Map<string, string> {
 
 function safeFoods(foods: Food[]): Set<string> {
   const allergens = new Set(findAllergens(foods).values());
-  const safe = new Set<string>();
-  for (const food of foods) {
-    [...food.ingredients]
-      .filter((ingr) => !allergens.has(ingr))
-      .forEach((ingr) => safe.add(ingr));
-  }
-  return safe;
+  const allIngredients = foods.reduce((ingrs, food) => {
+    [...food.ingredients].map((ingr) => ingrs.add(ingr));
+    return ingrs;
+  }, new Set<string>());
+  return difference(allIngredients, allergens);
 }
 
 function part1(foods: Food[]): number {
-  const safe = safeFoods(foods)
+  const safe = safeFoods(foods);
   let safeCount = 0;
   for (const food of foods) {
     for (const ingr of food.ingredients) {
@@ -71,15 +72,18 @@ function part2(foods: Food[]): string {
   const allergens = findAllergens(foods);
   const keys = [...allergens.keys()];
   keys.sort();
-  return keys.map(k => allergens.get(k)).join(',')
+  return keys.map((k) => allergens.get(k)).join(',');
 }
 
 const exampleFoods = parseFoods(loadDayLines(21, 'example'));
-example.deepEqual(new Set(['kfcds', 'nhms', 'sbzzf', 'trh']), safeFoods(exampleFoods));
+example.deepEqual(
+  new Set(['kfcds', 'nhms', 'sbzzf', 'trh']),
+  safeFoods(exampleFoods)
+);
 example.equal(5, part1(exampleFoods));
 example.equal('mxmxvkd,sqjhc,fvjkl', part2(exampleFoods));
 
-const foods = parseFoods(loadDayLines(21))
+const foods = parseFoods(loadDayLines(21));
 answers(
   () => part1(foods),
   () => part2(foods)
