a412e5543c9f2b272c21779e3247e8dd2a9085ba
Zak Johnson <me@zakj.net>
2021-01-14 21:49:38 -0800

    2016 day 3

---
+++ 2016/day03.ts
@@ -0,0 +1,20 @@
+import { answers, load } from './advent';
+import { chunks, sum, zip } from './util';
+
+function parse(lines: string[]): number[][] {
+  return lines.map((line) => line.trim().split(/\s+/).map(Number));
+}
+
+function isValidTriangle(sides: number[]): boolean {
+  return sides.every((s, i) => s < sum(sides.filter((_, j) => i !== j)));
+}
+
+function fromColumns(lines: number[][]): number[][] {
+  return chunks(zip(...lines).flat(), 3)
+}
+
+const triangles = parse(load(3).lines);
+answers(
+  () => triangles.filter(isValidTriangle).length,
+  () => fromColumns(triangles).filter(isValidTriangle).length
+)
--- 2016/util.ts
+++ 2016/util.ts
@@ -1,3 +1,13 @@
+export function chunks<T>(arr: T[], chunkSize: number): T[][] {
+  const output = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    output.push(arr.slice(i, i + chunkSize));
+  }
+  return output;
+}
+
+export const sum = (xs: number[]): number => xs.reduce((acc, x) => acc + x, 0);
+
 export function range(start: number, stop: number): number[] {
   const step = start < stop ? 1 : -1
   let length = Math.abs(start - stop);
@@ -7,4 +17,8 @@ export function range(start: number, stop: number): number[] {
     start += step;
   }
   return xs;
+}
+
+export function zip<T>(...arrs: T[][]): T[][] {
+  return range(0, arrs[0].length).map((i) => arrs.map((x) => x[i]));
 }
