f286835e412efa4fec76dbd750d78cd28606755c
Zak Johnson <me@zakj.net>
2021-01-19 21:17:14 -0800

    2016 day 15

---
+++ 2016/day15.ts
@@ -0,0 +1,32 @@
+import { answers, example, load } from './advent';
+
+type Disc = { positions: number; time0: number };
+
+function parse(lines: string[]): Disc[] {
+  const re = /(\d+) positions.*position (\d+)\./;
+  return lines.map((line) => {
+    const match = line.match(re);
+    const [positions, time0] = [Number(match[1]), Number(match[2])];
+    return { positions, time0 };
+  });
+}
+
+function isOpenAt(d: Disc, t: number): boolean {
+  return (d.time0 + t) % d.positions === 0;
+}
+
+function firstButtonPress(discs: Disc[]): number {
+  let time = 0;
+  for (let time = 0; ; ++time) {
+    if (discs.every((d, i) => isOpenAt(d, time + i + 1))) return time;
+  }
+}
+
+const exampleDiscs = parse(load(15, 'ex').lines);
+example.equal(firstButtonPress(exampleDiscs), 5);
+
+const discs = parse(load(15).lines);
+answers(
+  () => firstButtonPress(discs),
+  () => firstButtonPress(discs.concat({ positions: 11, time0: 0 }))
+);
