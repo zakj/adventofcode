193fdfade69333f8b9d3d02b20995bf387d59dac
Zak Johnson <me@zakj.net>
2021-01-23 12:12:24 -0800

    Clean up advent.ts

--- 2016/advent.ts
+++ 2016/advent.ts
@@ -56,11 +56,11 @@ export function answers(...fns: (() => any)[]): void {
         ? `${(durationMs / 1000).toFixed(3)}s`
         : `${durationMs.toFixed(3)}m`;
     const indexStr = `${i + 1}: `;
-    process.stdout.write(indexStr)
-    process.stdout.cursorTo(process.stdout.columns - duration.length)
-    process.stdout.write(color('grey', duration))
-    process.stdout.cursorTo(indexStr.length)
-    console.log(color('green', result.toString()));
+    process.stdout.write(indexStr);
+    process.stdout.cursorTo(process.stdout.columns - duration.length);
+    process.stdout.write(color('grey', duration));
+    process.stdout.cursorTo(indexStr.length);
+    console.log(color('green', result?.toString()));
   });
 }
 
