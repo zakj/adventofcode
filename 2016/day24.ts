bc38bb4082ecbbf60f62ade5a4f5fa2429ee1389
Zak Johnson <me@zakj.net>
2021-01-24 11:12:00 -0800

    Optimize 2016 day 24
    Rather than search the entire space, convert into the traveling salesman
    problem: find the shortest paths between all relevant points, then find
    the shortest permutation of those paths.
    
    part1 4.7s -> 200ms
    part2 11.1s -> 200ms

--- 2016/day24.ts
+++ 2016/day24.ts
@@ -1,30 +1,31 @@
 import { answers, example, load } from './advent';
+import { combinations, permutations } from './util';
 
 type Point = { x: number; y: number };
 type PointHash = string;
+type TwoPointHash = string;
+const pointHash = (p: Point): PointHash => [p.x, p.y].join(',');
+const distanceHash = (a: Point, b: Point): TwoPointHash =>
+  [a, b].map(pointHash).join('|');
 
 type Map = {
   start: Point;
-  goals: Set<PointHash>;
+  goals: Point[];
   walls: Set<PointHash>;
 };
 
 function parse(lines: string[]): Map {
-  const map: Map = { start: null, goals: new Set(), walls: new Set() };
+  const map: Map = { start: null, goals: [], walls: new Set() };
   lines.forEach((line, row) => {
     line.split('').forEach((c, col) => {
       const point = { x: col, y: row };
       if (c === '0') map.start = point;
       else if (c === '#') map.walls.add(pointHash(point));
-      else if (c.match(/\d/)) map.goals.add(pointHash(point));
+      else if (c.match(/\d/)) map.goals.push(point);
     });
   });
   return map;
 }
-
-const pointHash = (p: Point): PointHash => [p.x, p.y].join(',');
-const stateHash = (p: Point, goals: Set<string>): string =>
-  JSON.stringify([pointHash(p), [...goals.values()].sort()]);
 
 function neighbors(p: Point, map: Map): Point[] {
   return [-1, 1]
@@ -35,34 +36,51 @@ function neighbors(p: Point, map: Map): Point[] {
     .filter((p) => !map.walls.has(pointHash(p)));
 }
 
-function shortestPath(map: Map, returnToStart = false): Point[] {
-  const visited = new Set<string>();
-  const q = [{ path: [map.start], goals: new Set<PointHash>() }];
+function shortestPath(map: Map, start: Point, goal: Point): number {
+  const visited = new Set<PointHash>();
+  const q: [number, Point][] = [[0, start]];
   while (q.length) {
-    const { path, goals } = q.shift();
-    if (
-      goals.size === map.goals.size &&
-      (!returnToStart ||
-        pointHash(path[path.length - 1]) === pointHash(map.start))
-    )
-      return path;
-    for (let point of neighbors(path[path.length - 1], map)) {
-      const newPath = path.concat(point);
-      const newGoals = new Set(goals);
-      if (map.goals.has(pointHash(point))) newGoals.add(pointHash(point));
-      if (visited.has(stateHash(point, newGoals))) continue;
-      visited.add(stateHash(point, newGoals));
-      q.push({ path: newPath, goals: newGoals });
+    const [steps, cur] = q.shift();
+    if (pointHash(cur) === pointHash(goal)) return steps;
+    for (const neighbor of neighbors(cur, map)) {
+      const hash = pointHash(neighbor);
+      if (visited.has(hash)) continue;
+      visited.add(hash);
+      q.push([steps + 1, neighbor]);
     }
   }
-  return [];
+}
+
+function solve(map: Map, returnToStart = false): number {
+  const distances = new Map<TwoPointHash, number>();
+  // Convert into traveling salesman problem.
+  for (const [a, b] of combinations([].concat(map.start, map.goals))) {
+    const hash1 = distanceHash(a, b);
+    const hash2 = distanceHash(b, a);
+    if (distances.has(hash1)) continue;
+    const d = shortestPath(map, a, b);
+    distances.set(hash1, d);
+    distances.set(hash2, d);
+  }
+  let min = Infinity;
+  for (const path of permutations(map.goals)) {
+    let d = 0;
+    let cur = map.start;
+    for (const goal of path) {
+      d += distances.get(distanceHash(cur, goal));
+      cur = goal;
+    }
+    if (returnToStart) d += distances.get(distanceHash(cur, map.start));
+    min = Math.min(min, d);
+  }
+  return min;
 }
 
 const exampleMap = parse(load(24, 'ex').lines);
-example.equal(shortestPath(exampleMap).length - 1, 14);
+example.equal(solve(exampleMap), 14);
 
 const map = parse(load(24).lines);
 answers(
-  () => shortestPath(map).length - 1,
-  () => shortestPath(map, true).length - 1
+  () => solve(map),
+  () => solve(map, true)
 );
--- 2016/util.ts
+++ 2016/util.ts
@@ -55,6 +55,28 @@ export function pairs<T>(arr: T[]): T[][] {
   return a.map((x, i) => [x, b[i]]);
 }
 
+export function* permutations<T>(arr: T[]): Generator<T[]> {
+  const length = arr.length;
+  const c = Array(length).fill(0);
+  let i = 1;
+
+  yield arr.slice();
+  while (i < length) {
+    if (c[i] < i) {
+      const k = i % 2 && c[i];
+      const p = arr[i];
+      arr[i] = arr[k];
+      arr[k] = p;
+      ++c[i];
+      i = 1;
+      yield arr.slice();
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+}
+
 export class PriorityQueue<T> {
   private items: [T, number][];
 
