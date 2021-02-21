d2672ab609468964a79fa844dc151174dc7d61db
Zak Johnson <me@zakj.net>
2021-02-20 22:06:19 -0800

    2019 day 4

--- 2019/day04.ts
+++ 2019/day04.ts
@@ -1,69 +1,29 @@
-import { pairs } from './util';
+import { answers, load } from '../advent';
+import { pairs, range } from '../util';
 
-const data = '197487-673251';
-const [start, end] = data.split('-').map(x => parseInt(x, 10));
-
-type MatchingCriteria = (x: number, start: number, end: number) => boolean;
-
-const matchesCriteriaPart1: MatchingCriteria = function (x, start, end) {
-  return (
-    x.toString().length === 6 &&
-    x >= start &&
-    x <= end &&
-    hasTwoSameAdjacentDigits(x) &&
-    !hasDecreasingDigits(x)
-  );
-}
-
-const matchesCriteriaPart2: MatchingCriteria = function (x, start, end) {
-  return (
-    matchesCriteriaPart1(x, start, end) && hasExactlyTwoSameAdjacentDigits(x)
-  );
-}
-
-function hasTwoSameAdjacentDigits(x: number): boolean {
-  return pairs(x.toString().split('')).reduce<boolean>(
-    (found, [a, b]) => found || a === b,
-    false
-  );
-}
-
-function hasExactlyTwoSameAdjacentDigits(x: number): boolean {
-  const digits = x.toString().split('');
-  let lastDigit = digits[0];
-  let run = 1;
-  let found = false;
-  digits.slice(1).forEach(digit => {
-    if (digit !== lastDigit) {
-      if (run === 2) {
-        found = true;
-      }
-      run = 1;
-      lastDigit = digit;
-    } else {
-      run++;
-    }
-  });
-  if (run === 2) found = true;
-  return found;
-}
-
-function hasDecreasingDigits(x: number) {
-  return pairs(
-    x
-      .toString()
-      .split('')
-      .map(x => parseInt(x, 10))
-  ).reduce((found, [a, b]) => found || b < a, false);
-}
-
-function matchingNumbersInRange(start: number, end: number, test: MatchingCriteria) {
-  let matches = [];
-  for (let i = start; i <= end; ++i) {
-    if (test(i, start, end)) matches.push(i);
+function isValid1(n: number): boolean {
+  const digits = n.toString().split('');
+  let hasPair = false;
+  for (const [a, b] of pairs(digits)) {
+    if (b < a) return false;
+    if (a === b) hasPair = true;
   }
-  return matches;
+  return hasPair;
 }
 
-console.log(matchingNumbersInRange(start, end, matchesCriteriaPart1).length);
-console.log(matchingNumbersInRange(start, end, matchesCriteriaPart2).length);
+function isValid2(n: number): boolean {
+  const digits = n.toString().split('');
+  const runs = new Map();
+  for (const [a, b] of pairs(digits)) {
+    if (b < a) return false;
+    if (a === b) runs.set(a, (runs.get(a) || 1) + 1);
+  }
+  return [...runs.values()].includes(2);
+}
+
+const [low, high] = load(4).lines[0].split('-').map(Number);
+answers.expect(1640, 1126);
+answers(
+  () => range(low, high + 1).filter(isValid1).length,
+  () => range(low, high + 1).filter(isValid2).length
+);
