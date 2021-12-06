05716825b5fcfca9bed208cb8728b82d506c5499
Zak Johnson <me@zakj.net>
2021-12-05 21:27:04 -0800

    2021 day 6 cleanup

--- 2021/day06.ts
+++ 2021/day06.ts
@@ -5,44 +5,25 @@ function parse(input: string): number[] {
   return input.trim().split(',').map(Number);
 }
 
-function simulateNaive(fish: number[], days: number): number[] {
-  for (let day = 0; day < days; ++day) {
-    const newFish = [];
-
-    const startFishLength = fish.length;
-    for (let i = 0; i < startFishLength; ++i) {
-      if (fish[i] === 0) {
-        fish[i] = 6;
-        fish.push(8);
-      } else {
-        fish[i]--;
-      }
-    }
-  }
-  return fish;
-}
-
 function simulate(fish: number[], days: number): number {
   const counts = new Array(9).fill(0);
-  fish.forEach((f) => {
-    counts[f]++;
-  });
+  fish.forEach((f) => counts[f]++);
   for (let day = 0; day < days; ++day) {
     const zeros = counts.shift();
     counts[6] += zeros;
-    counts[8] = zeros;
+    counts.push(zeros);
   }
   return sum(counts);
 }
 
 const exampleFish = parse(load(6, 'ex').raw);
-example.equal(simulate(exampleFish.slice(), 18), 26);
-example.equal(simulate(exampleFish.slice(), 80), 5934);
-example.equal(simulate(exampleFish.slice(), 256), 26984457539);
+example.equal(simulate(exampleFish, 18), 26);
+example.equal(simulate(exampleFish, 80), 5934);
+example.equal(simulate(exampleFish, 256), 26984457539);
 
 const fish = parse(load(6).raw);
 answers.expect(352195);
 answers(
-  () => simulate(fish.slice(), 80),
-  () => simulate(fish.slice(), 256)
+  () => simulate(fish, 80),
+  () => simulate(fish, 256)
 );
