491e5c3b0eb6a72d03578aed4fc745e7b51e5021
Zak Johnson <me@zakj.net>
2021-12-06 21:32:08 -0800

    2021 day 7 cleanup

--- 2021/day07.ts
+++ 2021/day07.ts
@@ -1,41 +1,22 @@
 import { answers, load } from '../advent';
 import { sum } from '../util';
 
-function alignCrabs(crabs: number[]): number {
+function alignCrabs(
+  crabs: number[],
+  distanceCost: (distance: number) => number
+): number {
   const maxPos = Math.max(...crabs);
-  let bestPos = null;
   let bestCost = Infinity;
   for (let i = 0; i <= maxPos; ++i) {
-    const cost = sum(crabs.map((c) => Math.abs(i - c)));
-    if (cost < bestCost) {
-      bestCost = cost;
-      bestPos = i;
-    }
+    const cost = sum(crabs.map((c) => distanceCost(Math.abs(i - c))));
+    if (cost < bestCost) bestCost = cost;
   }
   return bestCost;
-}
-
-function alignCrabs2(crabs: number[]): number {
-  const maxPos = Math.max(...crabs);
-  let bestPos = null;
-  let bestCost = Infinity;
-  for (let i = 0; i <= maxPos; ++i) {
-    const cost = sum(crabs.map((c) => sumOfConsInts(Math.abs(i - c))));
-    if (cost < bestCost) {
-      bestCost = cost;
-      bestPos = i;
-    }
-  }
-  return bestCost;
-}
-
-function sumOfConsInts(x: number): number {
-  return (x * (1 + x)) / 2;
 }
 
 const crabs = load(7).raw.split(',').map(Number);
-answers.expect(357353);
+answers.expect(357353, 104822130);
 answers(
-  () => alignCrabs(crabs),
-  () => alignCrabs2(crabs)
+  () => alignCrabs(crabs, (d) => d),
+  () => alignCrabs(crabs, (d) => (d * (1 + d)) / 2)
 );
