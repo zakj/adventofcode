4f12c25e2cc233f24397a82b4441d4ed8942645d
Zak Johnson <me@zakj.net>
2021-12-08 18:55:42 -0800

    Improved 2021 day 8

--- 2021/day08.ts
+++ 2021/day08.ts
@@ -1,161 +1,74 @@
 import { answers, example, load } from '../advent';
-import { XSet } from '../util';
+import { sum } from '../util';
 
-function parse(lines: string[]) {
+type PatternEntry = {
+  input: string[];
+  output: string[];
+};
+
+function parse(lines: string[]): PatternEntry[] {
   return lines.map((line) => {
     const [input, output] = line.split(' | ');
     return { input: input.split(' '), output: output.split(' ') };
   });
 }
 
-const MAP = {
-  0: 'abcefg',
-  1: 'cf',
-  2: 'acdeg',
-  3: 'acdfg',
-  4: 'bcdf',
-  5: 'abdfg',
-  6: 'abdefg',
-  7: 'acf',
-  8: 'abcdefg',
-  9: 'abcdfg',
-};
-const REVMAP = new Map(Object.entries(MAP).map(([k, v]) => [v, k]));
+type Segment = Set<string>;
+const SEGMENTS: Segment[] = [
+  new Set('abcefg'),
+  new Set('cf'),
+  new Set('acdeg'),
+  new Set('acdfg'),
+  new Set('bcdf'),
+  new Set('abdfg'),
+  new Set('abdefg'),
+  new Set('acf'),
+  new Set('abcdefg'),
+  new Set('abcdfg'),
+];
 
-function mess(data: { input: string[]; output: string[] }[]) {
-  // sum of all output digits
-  let total = 0;
-  for (const line of data) {
-    const unknowns = [...line.input, ...line.output].map(
-      (x) => new XSet((h) => h, x)
-    );
-    const mapping = new Map<string, string>();
-    const possible = new Map<string, XSet<string>>(
-      'abcdefg'.split('').map((i) => [i, new XSet((h) => h, 'abcdefg')])
-    );
+function intersect<T>(a: Set<T>, b: Set<T>): Set<T> {
+  return new Set<T>([...a].filter((x) => b.has(x)));
+}
 
-    const mappings = new Array(10);
-    const cmp = (s: XSet<string>, d: number) => s.intersect(mappings[d]).size;
-    mappings[1] = unknowns.find((s) => s.size === MAP[1].length);
-    mappings[4] = unknowns.find((s) => s.size === MAP[4].length);
-    mappings[7] = unknowns.find((s) => s.size === MAP[7].length);
-    mappings[8] = unknowns.find((s) => s.size === MAP[8].length);
+function hashSegment(s: Segment, four: Segment, seven: Segment): string {
+  return `${s.size} ${intersect(s, four).size} ${intersect(s, seven).size}`;
+}
 
-    mappings[0] = unknowns.find(
-      (s) => s.size === MAP[6].length && cmp(s, 4) === 3 && cmp(s, 1) === 2
-    );
-    mappings[6] = unknowns.find(
-      (s) => s.size === MAP[6].length && cmp(s, 4) === 3 && cmp(s, 1) == 1
-    );
-    mappings[9] = unknowns.find(
-      (s) => s.size === MAP[9].length && cmp(s, 4) === 4 && cmp(s, 1) === 2
-    );
+const segmentHashes = new Map(
+  SEGMENTS.map((set, i) => [hashSegment(set, SEGMENTS[4], SEGMENTS[7]), i])
+);
 
-    mappings[2] = unknowns.find(
-      (s) => s.size === MAP[2].length && cmp(s, 6) === 4 && cmp(s, 1) === 1
-    );
-    mappings[3] = unknowns.find(
-      (s) => s.size === MAP[3].length && cmp(s, 6) === 4 && cmp(s, 1) === 2
-    );
-    mappings[5] = unknowns.find(
-      (s) => s.size === MAP[5].length && cmp(s, 6) === 5 && cmp(s, 1) === 1
-    );
-    // console.log(mappings);
-    // console.log('###');
-
-    const x = Number(
-      line.output
-        .map((x) => {
-          const s = new XSet((h) => h, x);
-          return mappings.findIndex(
-            (ms) => ms.size === s.size && ms.intersect(s).size === s.size
-          );
-        })
-        .join('')
-    );
-    if (x === 8384) {
-      console.log(line);
-      console.log(mappings);
-    }
-    console.log(x);
-    total += x;
-
-    // console.log(possible);
-    // for (const x of unknowns) {
-    //   [1, 4, 7].forEach((num) => {
-    //     if (x.length === MAP[num].length) {
-    //       for (const c of x) {
-    //         possible.set(
-    //           c,
-    //           possible.get(c).intersect(new XSet((h) => h, MAP[num]))
-    //         );
-    //       }
-
-    //       // console.log('found match for', x, num);
-    //       // for (let i = 0; i < x.length; ++i) {
-    //       //   if (x[i] === 'b') {
-    //       //     console.log('HERE', x[i], MAP[num][i]);
-    //       //   }
-    //       //   if (mapping.has(x[i]) && mapping.get(x[i]) !== MAP[num][i]) {
-    //       //     console.log(x);
-    //       //     throw 'oops';
-    //       //   }
-    //       //   mapping.set(x[i], MAP[num][i]);
-    //       // }
-    //       // console.log({ mapping });
-    //     }
-    //   });
-    // }
-    // while (![...possible.values()].every(s => s.size === 1)) {
-
-    // }
-
-    // console.log(possible);
-    // const mappedInput = line.input.slice();
-    // const xmappig = new XSet((h) => h, mapping.keys());
-    // const knowns = [];
-    // for (const x of unknowns) {
-    //   const xset = new XSet((h) => h, x);
-    //   if (xset.size === xset.intersect(xmappig).size) {
-    //     console.log('got one');
-    //   }
-    //   const test = new Array(x.length).fill('.');
-    //   for (let i = 0; i < x.length; ++i) {
-    //     const c = x[i];
-    //     if (mapping.has(c)) {
-    //       test[i] = mapping.get(c);
-    //     }
-    //   }
-    //   if (test.includes('.')) throw 'not found';
-    //   if (!REVMAP.has(test.join(''))) {
-    //     console.log({ mapping, REVMAP, test: test.join(''), x });
-    //     throw 'not found';
-    //   }
-    // }
-    // // while (mapping.size < 8) {}
-    // const output: number[] = [];
-    // total += sum(output);
-  }
-  return total;
+function decodedOutput(entries: PatternEntry[]) {
+  return sum(
+    entries.map((entry) => {
+      const four = new Set(
+        entry.input.find((s) => s.length === SEGMENTS[4].size)
+      );
+      const seven = new Set(
+        entry.input.find((s) => s.length === SEGMENTS[7].size)
+      );
+      return Number(
+        entry.output
+          .map((s) => segmentHashes.get(hashSegment(new Set(s), four, seven)))
+          .join('')
+      );
+    })
+  );
 }
 
 const exampleData = parse(load(8, 'ex1').lines);
-example.equal(mess(exampleData), 5353);
+example.equal(decodedOutput(exampleData), 5353);
 const exampleData2 = parse(load(8, 'ex2').lines);
-example.equal(mess(exampleData2), 61229);
+example.equal(decodedOutput(exampleData2), 61229);
 
 const data = parse(load(8).lines);
-answers.expect(479);
+answers.expect(479, 1041746);
 answers(
   () => {
-    let count = 0;
-    for (const line of data) {
-      count += line.output.filter((x) =>
-        [2, 3, 4, 7].includes(x.length)
-      ).length;
-    }
-    return count;
+    return data
+      .flatMap((entry) => entry.output)
+      .filter((x) => [2, 3, 4, 7].includes(x.length)).length;
   },
-  // 728591 too low
-  () => mess(data)
+  () => decodedOutput(data)
 );
