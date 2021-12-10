179c20ede05e8cfafd3c0d006c0bfc14346bbc73
Zak Johnson <me@zakj.net>
2021-12-09 21:49:26 -0800

    2021 day 10 cleanup

--- 2021/day10.ts
+++ 2021/day10.ts
@@ -1,97 +1,61 @@
 import { answers, load } from '../advent';
+import { sum } from '../util';
 
-// const exampleData = parse(load(10, 'ex').raw);
-// example.equal(XXX(exampleData), XXX);
+const PAIRS = new Map([
+  ['(', ')'],
+  ['[', ']'],
+  ['{', '}'],
+  ['<', '>'],
+]);
+
+const CORRUPT_SCORE = new Map([
+  [')', 3],
+  [']', 57],
+  ['}', 1197],
+  ['>', 25137],
+]);
+
+const AUTOCOMPLETE_SCORE = new Map([
+  [')', 1],
+  [']', 2],
+  ['}', 3],
+  ['>', 4],
+]);
+
+function corruptScore(line: string): number {
+  const openChars = [...PAIRS.keys()];
+  const stack = [];
+  for (const c of line) {
+    if (openChars.includes(c)) stack.push(c);
+    else if (c != PAIRS.get(stack.pop())) return CORRUPT_SCORE.get(c);
+  }
+  return 0;
+}
+
+function autoCompleteScore(line: string): number {
+  const openChars = [...PAIRS.keys()];
+  const closeChars = [...PAIRS.values()];
+  return line
+    .split('')
+    .reduce((stack, c) => {
+      if (openChars.includes(c)) stack.push(c);
+      else if (closeChars.includes(c)) stack.pop();
+      return stack;
+    }, [])
+    .map((c) => PAIRS.get(c))
+    .reverse()
+    .reduce((score, c) => score * 5 + AUTOCOMPLETE_SCORE.get(c), 0);
+}
 
 const lines = load(10).lines;
-answers.expect(367059);
+answers.expect(367059, 1952146692);
 answers(
+  () => sum(lines.map(corruptScore)),
   () => {
-    let score = 0;
-    for (const line of lines) {
-      const stack = [];
-      let corrupted = false;
-      for (const c of line) {
-        if ('([{<'.includes(c)) {
-          stack.push(c);
-        } else {
-          const last = stack.pop();
-          if (c === ')' && last !== '(') {
-            corrupted = true;
-            score += 3;
-          } else if (c === ']' && last !== '[') {
-            corrupted = true;
-            score += 57;
-          } else if (c === '}' && last !== '{') {
-            corrupted = true;
-            score += 1197;
-          } else if (c === '>' && last !== '<') {
-            corrupted = true;
-            score += 25137;
-          }
-        }
-      }
-    }
-    return score;
-  },
-  () => {
-    const incomplete = [];
-    for (const line of lines) {
-      const stack = [];
-      let corrupted = false;
-      for (const c of line) {
-        if ('([{<'.includes(c)) {
-          stack.push(c);
-        } else {
-          const last = stack.pop();
-          if (c === ')' && last !== '(') {
-            corrupted = true;
-          } else if (c === ']' && last !== '[') {
-            corrupted = true;
-          } else if (c === '}' && last !== '{') {
-            corrupted = true;
-          } else if (c === '>' && last !== '<') {
-            corrupted = true;
-          }
-        }
-      }
-      if (corrupted === false) incomplete.push(line);
-    }
-
-    const lineScores = [];
-    for (const line of incomplete) {
-      const stack = [];
-      for (const c of line) {
-        if ('([{<'.includes(c)) {
-          stack.push(c);
-        } else if (')]}>'.includes(c)) {
-          stack.pop();
-        } else {
-          throw 'wat';
-        }
-      }
-
-      const closing = [];
-      while (stack.length) {
-        const c = stack.pop();
-        if (c === '(') closing.push(')');
-        if (c === '{') closing.push('}');
-        if (c === '[') closing.push(']');
-        if (c === '<') closing.push('>');
-      }
-
-      let score = 0;
-      let scoreCount = 0;
-      for (const c of closing) {
-        score *= 5;
-        if (c === ')') score += 1;
-        if (c === ']') score += 2;
-        if (c === '}') score += 3;
-        if (c === '>') score += 4;
-      }
-      lineScores.push(score);
-    }
-    lineScores.sort((a, b) => a - b);
+    const lineScores = lines
+      .filter((line) => corruptScore(line) === 0)
+      .map(autoCompleteScore)
+      .sort((a, b) => a - b);
     return lineScores[Math.floor(lineScores.length / 2)];
   }
 );
