5eecb75be6220c53b9de1783383100e4a9b26e41
Zak Johnson <me@zakj.net>
2021-12-18 19:46:01 -0800

    Factor out ocr code from 2018 day 10

--- 2018/day10.ts
+++ 2018/day10.ts
@@ -39,48 +39,10 @@ function converge(rules: PointRule[]): number {
   }
 }
 
-function toString(points: Point[]): string {
-  const [min, max] = bounds(points);
-  const key = (p: Point): string => `${p[0]},${p[1]}`;
-  const ps = new Set(points.map(key));
-  const rows = [];
-  for (let y = min[1]; y <= max[1]; ++y) {
-    const row = [];
-    for (let x = min[0]; x <= max[0]; ++x) {
-      if (ps.has(key([x, y]))) row.push('#');
-      else row.push(' ');
-    }
-    rows.push(row.join(''));
-  }
-  return rows.join('\n');
-}
-
-function makeLetterMap() {
-  // figlet.txt from https://gist.github.com/usbpc/5fa0be48ad7b4b0594b3b8b029bc47b4
-  const chunks = readFileSync('./figlet.txt').toString().split('\n\n');
-  const values = chunks.shift().split('');
-  const keys = chunks.map((c) => c.split('\n').join(''));
-  return new Map(zip(keys, values) as [string, string][]);
-}
-
-function deFiglet(s: string): string {
-  const charWidth = 6;
-  const padding = 2;
-  const letterMap = makeLetterMap();
-  const rows = s.split('\n');
-  let x = 0;
-  const output = [];
-  while (x < rows[0].length) {
-    const key = rows.map((r) => r.slice(x, x + charWidth)).join('');
-    output.push(letterMap.get(key));
-    x += charWidth + padding;
-  }
-  return output.join('');
-}
-
 const rules = parse(load(10).lines);
 answers.expect('KFLBHXGK', 10659);
 answers(
-  () => deFiglet(toString(after(converge(rules), rules))),
+  () =>
+    ocr(toAscii(new PointSet(after(converge(rules), rules))), './figlet.txt'),
   () => converge(rules)
 );
