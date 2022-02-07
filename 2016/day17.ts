f7315102baf19c71bfa153dc1da75c0be53517d1
Zak Johnson <me@zakj.net>
2022-02-06 19:56:16 -0800

    Clean up 2016 day 17

--- 2016/day17.ts
+++ 2016/day17.ts
@@ -1,58 +1,57 @@
 import { example, load, solve } from 'lib/advent';
-import { md5, sum, zip } from 'lib/util';
+import { Dir, move, Point, PointMap } from 'lib/coords';
+import { md5 } from 'lib/util';
 
-type Point = [number, number];
 type Move = 'U' | 'D' | 'L' | 'R';
 
 const GRID_SIZE = 4;
-const DELTAS: Record<Move, Point> = {
-  U: [0, -1],
-  D: [0, +1],
-  L: [-1, 0],
-  R: [+1, 0],
+const MOVES: Record<Move, Dir> = {
+  U: Dir.Up,
+  D: Dir.Down,
+  L: Dir.Left,
+  R: Dir.Right,
 };
 
-function moves(passcode: string, p: Point, path: Move[]): Move[] {
-  const order = 'UDLR';
-  const hash = md5(passcode + path.join('')).slice(0, 4);
-  return [...hash]
-    .map((c, i) => ('bcdef'.includes(c) ? (order[i] as Move) : null))
+function moves(passcode: string, path: string): Move[] {
+  return [...md5(passcode + path).slice(0, 4)]
+    .map((c, i) => ('bcdef'.includes(c) ? ('UDLR'[i] as Move) : null))
     .filter(Boolean);
 }
 
-// TODO graph.search
+function inBounds(p: Point): boolean {
+  return p.x >= 0 && p.x < GRID_SIZE && p.y >= 0 && p.y < GRID_SIZE;
+}
+
 function shortestPath(passcode: string): string {
-  const goal = [GRID_SIZE - 1, GRID_SIZE - 1];
-  const q: { p: Point; path: Move[] }[] = [{ p: [0, 0] as Point, path: [] }];
+  const q: { p: Point; path: string }[] = [{ p: { x: 0, y: 0 }, path: '' }];
   while (q.length) {
     const cur = q.shift();
-    if (cur.p[0] === goal[0] && cur.p[1] === goal[1]) return cur.path.join('');
-    for (let move of moves(passcode, cur.p, cur.path)) {
-      const newPoint = zip(cur.p, DELTAS[move]).map(sum) as Point;
-      if (!newPoint.every((x) => x >= 0 && x < GRID_SIZE)) continue;
-      q.push({ p: newPoint, path: cur.path.concat(move) });
+    if (cur.p.x === GRID_SIZE - 1 && cur.p.x === cur.p.y) return cur.path;
+    for (const nextMove of moves(passcode, cur.path)) {
+      const next = move(cur.p, MOVES[nextMove]);
+      if (!inBounds(next)) continue;
+      q.push({ p: next, path: cur.path + nextMove });
     }
   }
 }
 
 function longestPath(passcode: string): string {
-  const goal = [GRID_SIZE - 1, GRID_SIZE - 1];
-  const q: { p: Point; path: Move[] }[] = [{ p: [0, 0] as Point, path: [] }];
-  const paths = new Map<string, Move[]>();
+  const goal = { x: GRID_SIZE - 1, y: GRID_SIZE - 1 };
+  const q: { p: Point; path: string }[] = [{ p: { x: 0, y: 0 }, path: '' }];
+  const paths = new PointMap<string>();
   while (q.length) {
     const cur = q.shift();
-    const key = cur.p.join(',');
-    const longestSeen = paths.get(key);
+    const longestSeen = paths.get(cur.p);
     if (longestSeen && longestSeen.length > cur.path.length) continue;
-    paths.set(key, cur.path);
-    if (cur.p[0] === goal[0] && cur.p[1] === goal[1]) continue;
-    for (let move of moves(passcode, cur.p, cur.path)) {
-      const newPoint = zip(cur.p, DELTAS[move]).map(sum) as Point;
-      if (!newPoint.every((x) => x >= 0 && x < GRID_SIZE)) continue;
-      q.push({ p: newPoint, path: cur.path.concat(move) });
+    paths.set(cur.p, cur.path);
+    if (cur.p.x === goal.x && cur.p.y === goal.y) continue;
+    for (const nextMove of moves(passcode, cur.path)) {
+      const next = move(cur.p, MOVES[nextMove]);
+      if (!inBounds(next)) continue;
+      q.push({ p: next, path: cur.path + nextMove });
     }
   }
-  return paths.get(goal.join(',')).join('');
+  return paths.get(goal);
 }
 
 example.equal(shortestPath('ihgpwlah'), 'DDRRRD');
