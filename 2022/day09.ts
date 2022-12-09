6034faa18f9eaadad25d0e8fcd33a380f1d31e9e
Zak Johnson <me@zakj.net>
2022-12-08 22:47:18 -0800

    2022 day 09 cleanup

--- 2022/day09.ts
+++ 2022/day09.ts
@@ -1,6 +1,5 @@
 import { example, load, solve } from 'lib/advent';
 import { Dir, move, Point, PointSet } from 'lib/coords';
-import { range } from 'lib/util';
 
 type Move = { dir: Dir; count: number };
 const dirMap = {
@@ -17,85 +16,38 @@ function parse(lines: string[]): Move[] {
   });
 }
 
-const exampleData = parse(
-  `R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2`.split('\n')
-);
-example.equal(13, part1(exampleData));
-example.equal(1, part2(exampleData));
+const exampleData = parse(load('ex').lines);
+example.equal(13, tailVisits(exampleData, 2));
+example.equal(1, tailVisits(exampleData, 10));
 
-function part1(moves: Move[]): number {
+function tailVisits(moves: Move[], n: number): number {
   const seen = new PointSet();
-  let head: Point = { x: 0, y: 0 };
-  let tail: Point = { x: 0, y: 0 };
-  seen.add(tail);
-
-  for (const { dir, count } of moves) {
-    for (let i = 0; i < count; ++i) {
-      head = move(head, dir);
-
-      // touching; do nothing
-      if (Math.abs(head.x - tail.x) < 2 && Math.abs(head.y - tail.y) < 2)
-        continue;
-
-      // same col, move horizontally/vert if in same col/row
-      if (head.y === tail.y) {
-        tail = move(tail, head.x > tail.x ? Dir.Right : Dir.Left);
-      } else if (head.x === tail.x) {
-        tail = move(tail, head.y > tail.y ? Dir.Down : Dir.Up);
-      } else {
-        // diagonal
-        tail = move(tail, head.x > tail.x ? Dir.Right : Dir.Left);
-        tail = move(tail, head.y > tail.y ? Dir.Down : Dir.Up);
-      }
-      seen.add(tail);
-    }
-  }
-
-  // console.log(toAscii(seen));
-  return seen.size;
-}
-
-function part2(moves: Move[]): number {
-  const knots = range(0, 10).map(() => ({ x: 0, y: 0 }));
-  const seen = new PointSet();
+  const knots: Point[] = Array(n).fill({ x: 0, y: 0 });
   seen.add(knots[0]);
+
   for (const { dir, count } of moves) {
     for (let i = 0; i < count; ++i) {
       knots[0] = move(knots[0], dir);
       for (let k = 1; k < knots.length; ++k) {
         const head = knots[k - 1];
         let tail = knots[k];
-        // touching; do nothing
         if (Math.abs(head.x - tail.x) < 2 && Math.abs(head.y - tail.y) < 2)
           continue;
-
-        // same col, move horizontally/vert if in same col/row
-        if (head.y === tail.y) {
+        if (head.x !== tail.x)
           tail = move(tail, head.x > tail.x ? Dir.Right : Dir.Left);
-        } else if (head.x === tail.x) {
+        if (head.y !== tail.y)
           tail = move(tail, head.y > tail.y ? Dir.Down : Dir.Up);
-        } else {
-          // diagonal
-          tail = move(tail, head.x > tail.x ? Dir.Right : Dir.Left);
-          tail = move(tail, head.y > tail.y ? Dir.Down : Dir.Up);
-        }
         knots[k] = tail;
       }
       seen.add(knots[knots.length - 1]);
     }
   }
+
   return seen.size;
 }
 
 const data = parse(load().lines);
 export default solve(
-  () => part1(data),
-  () => part2(data)
+  () => tailVisits(data, 2),
+  () => tailVisits(data, 10)
 ).expect(6271, 2458);
