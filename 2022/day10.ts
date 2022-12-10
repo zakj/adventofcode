00aa117cf656b32388b5008b45b57031381485ef
Zak Johnson <me@zakj.net>
2022-12-10 11:03:44 -0800

    2022 day 10 cleanup

--- 2022/day10.ts
+++ 2022/day10.ts
@@ -1,5 +1,6 @@
 import { example, load, ocr, solve } from 'lib/advent';
 import { Point, PointSet, toAscii } from 'lib/coords';
+import { Iter, iter } from 'lib/iter';
 
 function parse(lines: string[]) {
   return lines.map((line) => {
@@ -9,77 +10,46 @@ function parse(lines: string[]) {
 }
 
 type Instruction = { instr: string; arg: number };
+type Cycle = { cycle: number; X: number };
+type CRT = PointSet;
 
-function part1(instructions: Instruction[]) {
-  let X = 1;
-  let cycle = 1;
-  const values = [0];
-  const nextCycle = () => {
-    values.push(X);
-    cycle += 1;
-  };
-  for (const { instr, arg } of instructions) {
-    if (instr === 'noop') nextCycle();
-    else if (instr === 'addx') {
-      nextCycle();
-      nextCycle();
-      X += arg;
-    }
-  }
-  let sum = 0;
-  for (let i = 20; i <= 220; i += 40) {
-    // console.log({ cycle: i, X: values[i] });
-    sum += i * values[i];
-  }
-  return sum;
+function registerValues(instructions: Instruction[]): Iter<Cycle> {
+  return iter(instructions)
+    .scan(
+      (prev, { instr, arg }) => {
+        const X = prev[prev.length - 1];
+        return [X, ...(instr === 'addx' ? [X + arg] : [])];
+      },
+      [1]
+    )
+    .flat()
+    .map((X, i) => ({ X, cycle: i + 1 }));
 }
 
-function part2(instructions: Instruction[]) {
-  let X = 1;
-  let cycle = 0;
-  const values = [];
+function signalStrengths(instructions: Instruction[]): number {
+  return registerValues(instructions)
+    .filter(({ cycle }) => (cycle % 40) - 20 === 0)
+    .map(({ X, cycle }) => X * cycle)
+    .sum();
+}
 
-  const iToP = (i: number): Point => {
-    const y = Math.floor(i / 40);
-    const x = i % 40;
-    return { x, y };
-  };
+function drawCrt(instructions: Instruction[]): CRT {
+  const toPoint = (i: number): Point => ({ x: i % 40, y: Math.floor(i / 40) });
 
-  const nextCycle = () => {
-    values.push(X);
-    // middle of 3px wide sprite is at X
-
-    const pixel = iToP(cycle);
-    const sprite = iToP(X);
-    if (Math.abs(sprite.x - pixel.x) <= 1) {
-      crt.add(pixel);
-    }
-
-    cycle += 1;
-    cycle %= 240;
-    console.log({ cycle, pixel, sprite, X });
-  };
-
-  const crt = new PointSet();
-  for (const { instr, arg } of instructions) {
-    if (instr === 'noop') nextCycle();
-    else if (instr === 'addx') {
-      nextCycle();
-      nextCycle();
-      X += arg;
-    }
-  }
-
-  return crt;
+  return registerValues(instructions).reduce((crt, { cycle, X }) => {
+    const pixel = toPoint(cycle - 1);
+    const sprite = toPoint(X);
+    if (Math.abs(sprite.x - pixel.x) <= 1) crt.add(pixel);
+    return crt;
+  }, new PointSet());
 }
 
 const exampleData = parse(load('ex').lines);
-example.equal(13140, part1(exampleData));
-console.log(part2(exampleData));
+example.equal(13140, signalStrengths(exampleData));
+// console.log(toAscii(drawCrt(exampleData)));
 
 const data = parse(load().lines);
-// console.log(data);
 export default solve(
-  () => part1(data),
-  () => ocr(toAscii(part2(data)), '4x6')
+  () => signalStrengths(data),
+  () => ocr(toAscii(drawCrt(data)), '4x6')
 ).expect(13740, 'ZUPRFECL');
