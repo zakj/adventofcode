7e964d6c9a9945708373aaff0998f2204fec07f5
Zak Johnson <me@zakj.net>
2022-12-12 22:06:30 -0800

    2022 day 13 cleanup

--- 2022/day13.ts
+++ 2022/day13.ts
@@ -1,61 +1,53 @@
 import { example, load, solve } from 'lib/advent';
+import { Iter, iter } from 'lib/iter';
 
-const CONTINUE = Symbol('continue');
+type Packet = number | (Packet | number)[];
+type Pair = [Packet, Packet];
 
-function parse(paras: string[][]) {
+function parse(paras: string[][]): Pair[] {
   return paras.map((lines) => {
-    return lines.map((line) => JSON.parse(line));
+    const [a, b] = lines;
+    return [JSON.parse(a), JSON.parse(b)];
   });
 }
 
+function packetOrder(left: Packet, right: Packet): number {
+  if (typeof left === 'number' && typeof right === 'number')
+    return left - right;
+
+  if (!Array.isArray(left)) left = [left];
+  if (!Array.isArray(right)) right = [right];
+
+  const length = Math.min(left.length, right.length);
+  for (let i = 0; i < length; ++i) {
+    const result = packetOrder(left[i], right[i]);
+    if (result !== 0) return result;
+  }
+  return left.length - right.length;
+}
+
+function orderedIndexes(pairs: Pair[]): Iter<number> {
+  return iter(pairs)
+    .map(([left, right], i) => [packetOrder(left, right), i + 1])
+    .filter(([order]) => order < 0)
+    .map(([, index]) => index);
+}
+
+function decoderKey(pairs: Pair[], a: Packet, b: Packet): number {
+  const packets = [a, b, ...pairs.flat(1)];
+  packets.sort(packetOrder);
+  return (
+    (packets.findIndex((x) => x === a) + 1) *
+    (packets.findIndex((x) => x === b) + 1)
+  );
+}
+
 const exampleData = parse(load('ex').paragraphs);
-example.equal(13, part1(exampleData));
-example.equal(140, part2(exampleData));
-
-function isRightOrder(left, right) {
-  if (typeof left === 'number' && typeof right === 'number') {
-    if (left < right) return true;
-    if (left > right) return false;
-    return CONTINUE;
-  } else {
-    if (!Array.isArray(left)) left = [left];
-    if (!Array.isArray(right)) right = [right];
-    const length = Math.max(left.length, right.length);
-    for (let i = 0; i < length; ++i) {
-      const [l, r] = [left[i], right[i]];
-      if (l === undefined) return true;
-      if (r === undefined) return false;
-      const result = isRightOrder(left[i], right[i]);
-      if (result === CONTINUE) continue;
-      return result;
-    }
-  }
-  return CONTINUE;
-}
-
-function part1(pairs) {
-  let sum = 0;
-  for (let i = 0; i < pairs.length; ++i) {
-    const [left, right] = pairs[i];
-    const r = isRightOrder(left, right);
-    if (r) sum += i + 1;
-  }
-  return sum;
-}
-
-function part2(pairs) {
-  const dividerA = [[2]];
-  const dividerB = [[6]];
-  const packets = pairs.flat(1);
-  packets.push(dividerA, dividerB);
-  packets.sort((a, b) => (isRightOrder(a, b) ? -1 : 1));
-  const ai = packets.findIndex((x) => x === dividerA) + 1;
-  const bi = packets.findIndex((x) => x === dividerB) + 1;
-  return ai * bi;
-}
+example.equal(13, orderedIndexes(exampleData).sum());
+example.equal(140, decoderKey(exampleData, [[2]], [[6]]));
 
 const data = parse(load().paragraphs);
 export default solve(
-  () => part1(data),
-  () => part2(data)
+  () => orderedIndexes(data).sum(),
+  () => decoderKey(data, [[2]], [[6]])
 ).expect(5503, 20952);
