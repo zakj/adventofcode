a0d8ebe62dee3ecbcdb52ed6d8deda3871290f72
Zak Johnson <me@zakj.net>
2022-12-14 20:17:34 -0800

    2022 day 14 cleanup

--- 2022/day14.ts
+++ 2022/day14.ts
@@ -1,6 +1,15 @@
 import { example, load, solve } from 'lib/advent';
 import { Dir, move, Point, PointMap } from 'lib/coords';
 import { iter } from 'lib/iter';
+import { ValuesOf } from 'lib/util';
+
+const EMITTER = { x: 500, y: 0 };
+const Cell = {
+  ROCK: '#',
+  SAND: 'o',
+} as const;
+type Cell = ValuesOf<typeof Cell>;
+type Cave = PointMap<Cell>;
 
 function parse(lines: string[]) {
   return lines.map((line) => {
@@ -11,108 +20,83 @@ function parse(lines: string[]) {
   });
 }
 
-function buildMap(segments: Point[][]) {
-  const map = new PointMap<string>();
+function makeCave(segments: Point[][]): Cave {
+  const cave = new PointMap<Cell>();
   for (const points of segments) {
-    const cur = points[0];
-    map.set(cur, '#');
+    const cur = { ...points[0] };
+    cave.set(cur, Cell.ROCK);
     for (const point of points.slice(1)) {
-      if (cur.x === point.x) {
-        while (cur.y !== point.y) {
-          cur.y += Math.sign(point.y - cur.y);
-          map.set(cur, '#');
-        }
-      } else {
-        while (cur.x !== point.x) {
-          cur.x += Math.sign(point.x - cur.x);
-          map.set(cur, '#');
-        }
+      while (cur.x !== point.x) {
+        cur.x += Math.sign(point.x - cur.x);
+        cave.set(cur, Cell.ROCK);
+      }
+      while (cur.y !== point.y) {
+        cur.y += Math.sign(point.y - cur.y);
+        cave.set(cur, Cell.ROCK);
       }
     }
   }
-  return map;
+  return cave;
 }
 
-function restingSand(map: PointMap<string>): number {
-  const sandEmitter = { x: 500, y: 0 };
-  const bottom = iter(map.keys())
+function overflowCave(cave: Cave): Cave {
+  const bottom = iter(cave.keys())
     .map((p) => p.y)
     .max();
-  for (;;) {
-    let sand = move(sandEmitter, Dir.Down);
 
+  for (;;) {
+    let sand = move(EMITTER, Dir.Down);
     for (;;) {
       const down = move(sand, Dir.Down);
-      const downLeft = move(down, Dir.Left);
-      const downRight = move(down, Dir.Right);
-
-      if (down.y > bottom) {
-        return map.values().filter((v) => v === 'o').length;
-      } else if (!map.has(down)) {
-        sand = down;
-        continue;
-      } else if (!map.has(downLeft)) {
-        sand = downLeft;
-        continue;
-      } else if (!map.has(downRight)) {
-        sand = downRight;
-        continue;
-      } else {
-        map.set(sand, 'o');
+      if (down.y > bottom) return cave;
+      const fall = [down, move(down, Dir.Left), move(down, Dir.Right)].find(
+        (p) => !cave.has(p)
+      );
+      if (fall) sand = fall;
+      else {
+        cave.set(sand, Cell.SAND);
         break;
       }
     }
   }
-  return 0;
 }
 
-function restingSand2(map: PointMap<string>): number {
-  const sandEmitter = { x: 500, y: 0 };
+function fillCave(cave: Cave): Cave {
   const bottom =
-    iter(map.keys())
+    iter(cave.keys())
       .map((p) => p.y)
       .max() + 2;
 
-  const mapHas = (p: Point): boolean => p.y === bottom || map.has(p);
-
-  for (;;) {
-    if (map.has(sandEmitter)) {
-      return map.values().filter((v) => v === 'o').length;
-    }
-    let sand = sandEmitter;
-
+  while (!cave.has(EMITTER)) {
+    let sand = EMITTER;
     for (;;) {
       const down = move(sand, Dir.Down);
-      const downLeft = move(down, Dir.Left);
-      const downRight = move(down, Dir.Right);
-
-      if (!mapHas(down)) {
-        sand = down;
-        continue;
-      } else if (!mapHas(downLeft)) {
-        sand = downLeft;
-        continue;
-      } else if (!mapHas(downRight)) {
-        sand = downRight;
-        continue;
-      } else {
-        map.set(sand, 'o');
+      const fall = [down, move(down, Dir.Left), move(down, Dir.Right)].find(
+        (p) => p.y < bottom && !cave.has(p)
+      );
+      if (fall) sand = fall;
+      else {
+        cave.set(sand, Cell.SAND);
         break;
       }
     }
   }
-  return 0;
+  return cave;
 }
 
-const exampleData = [
+function countSand(cave: Cave): number {
+  return cave.values().filter((v) => v === Cell.SAND).length;
+}
+
+const exampleData = parse([
   '498,4 -> 498,6 -> 496,6',
   '503,4 -> 502,4 -> 502,9 -> 494,9',
-];
-example.equal(24, restingSand(buildMap(parse(exampleData))));
-example.equal(93, restingSand2(buildMap(parse(exampleData))));
+]);
+example.equal(24, countSand(overflowCave(makeCave(exampleData))));
+example.equal(93, countSand(fillCave(makeCave(exampleData))));
 
 const data = parse(load().lines);
 export default solve(
-  () => restingSand(buildMap(data)),
-  () => restingSand2(buildMap(parse(load().lines)))
+  () => countSand(overflowCave(makeCave(data))),
+  () => countSand(fillCave(makeCave(data)))
 ).expect(979, 29044);
