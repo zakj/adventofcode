2c2a7201a839188cf15b481b433b9fa27be86ed3
Zak Johnson <me@zakj.net>
2023-11-11 19:28:57 -0800

    py: Fix typing for parse.paras

--- py/parse.py
+++ py/parse.py
@@ -1,7 +1,6 @@
 import re
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, overload
 
-_identity = lambda x: x
 num_re = re.compile(r"[-+]?\d+")
 T = TypeVar("T")
 
@@ -17,10 +16,26 @@ def first_number(s: str) -> int:
     return int(match[0])
 
 
-def paras(s: str, mapper: Callable[[str], T] = _identity) -> list[list[T]]:
+@overload
+def paras(s: str, mapper: Callable[[str], T]) -> list[list[T]]:
+    ...
+
+
+@overload
+def paras(s: str) -> list[list[str]]:
+    ...
+
+
+# TODO: This feels like excessive fighting with the type system...
+def paras(
+    s: str, mapper: Callable[[str], T] | None = None
+) -> list[list[T]] | list[list[str]]:
     """Split a (multi-line) input string into a list of paragraphs.
 
     Each paragraph is a list of (single-line) strings, or the result of running
     `mapper` on each line.
     """
-    return [[mapper(x) for x in para.splitlines()] for para in s.split("\n\n")]
+    if mapper is None:
+        return [[x for x in para.splitlines()] for para in s.split("\n\n")]
+    else:
+        return [[mapper(x) for x in para.splitlines()] for para in s.split("\n\n")]
