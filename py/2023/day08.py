f8b14c927037680ea5f02f796d8e9b5b63689011
Zak Johnson <me@zakj.net>
2023-12-08 01:08:19 -0500

    py: 2023 day 8

---
+++ py/2023/day08.py
@@ -0,0 +1,50 @@
+import re
+from dataclasses import dataclass
+from itertools import cycle
+from math import lcm
+
+from aoc import main
+
+
+@dataclass
+class Node:
+    name: str
+    left: str
+    right: str
+
+
+def parse(s: str) -> tuple[str, dict[str, Node]]:
+    nodes = {}
+    [dirs, nodelist] = s.split("\n\n")
+    for line in nodelist.splitlines():
+        [name, left, right, *_] = re.findall(r"[A-Z]+", line)
+        nodes[name] = Node(name, left, right)
+    return dirs, nodes
+
+
+def steps(dirs: str, nodes: dict[str, Node]) -> int:
+    cur = nodes["AAA"]
+    for i, dir in enumerate(cycle(dirs), start=1):
+        cur = nodes[cur.left if dir == "L" else cur.right]
+        if cur.name == "ZZZ":
+            return i
+    return -1
+
+
+def ghost_steps(dirs: str, nodes: dict[str, Node]) -> int:
+    ends = []
+    for start in (n for n in nodes.keys() if n.endswith("A")):
+        cur = nodes[start]
+        for i, dir in enumerate(cycle(dirs), start=1):
+            cur = nodes[cur.left if dir == "L" else cur.right]
+            if cur.name.endswith("Z"):
+                ends.append(i)
+                break
+    return lcm(*ends)
+
+
+if __name__ == "__main__":
+    main(
+        lambda s: steps(*parse(s)),
+        lambda s: ghost_steps(*parse(s)),
+    )
