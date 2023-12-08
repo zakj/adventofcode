e829e29d458295479efc60f25824c3b194ef80b7
Zak Johnson <me@zakj.net>
2023-12-07 20:26:25 -0500

    py: speed up 2023 day 5

--- py/2023/day05.py
+++ py/2023/day05.py
@@ -1,105 +1,65 @@
 import re
-from itertools import count, islice
+from collections import defaultdict
+from functools import reduce
+from typing import Callable, NamedTuple
 
 from aoc import main
 from parse import all_numbers, paras
+from util import chunks
 
 
-def find_value_rev(dst: int, maps):
-    for dst_start, src_start, size in maps:
-        if dst >= dst_start and dst < dst_start + size:
-            return src_start + (dst - dst_start)
-    return dst
+class Map(NamedTuple):
+    dst: int
+    src: int
+    size: int
 
 
-def find_value(src: int, maps):
-    for dst_start, src_start, size in maps:
-        if src >= src_start and src < src_start + size:
-            return dst_start + (src - src_start)
-    return src
-
-
-def part1(s: str):
+def parse(s: str) -> tuple[list[int], list[list[Map]]]:
     chunks = paras(s)
     seeds = all_numbers(chunks.pop(0)[0])
 
-    fwd_map = {}
-    rev_map = {}
-    fwd_path = {}
-    rev_path = {}
+    path = {}
+    maps: dict[str, list[Map]] = defaultdict(list)
     for chunk in chunks:
         [src, dst, *_] = re.split(r"-to-| ", chunk.pop(0))
-        fwd_map[src] = []
-        fwd_path[src] = dst
-        rev_map[dst] = []
-        rev_path[dst] = src
+        path[src] = dst
         for line in chunk:
-            [dst_start, src_start, size] = all_numbers(line)
-            fwd_map[src].append((dst_start, src_start, size))
-            rev_map[dst].append((dst_start, src_start, size))
-        fwd_map[src].sort()
-        rev_map[dst].sort()
+            maps[src].append(Map(*all_numbers(line)))
 
-    found = []
-    for seed in seeds:
-        cur = "seed"
-        nn = seed
-        while cur != "location":
-            maps = fwd_map[cur]
-            nn = find_value(nn, maps)
-            cur = fwd_path[cur]
-        found.append(nn)
-    return min(found)
+    src = "seed"
+    nmaps: list[list[Map]] = []
+    while src != "location":
+        nmaps.append(maps[src])
+        src = path[src]
+
+    return seeds, nmaps
 
 
-def batched(iterable, n):
-    "Batch data into tuples of length n. The last batch may be shorter."
-    # batched('ABCDEFG', 3) --> ABC DEF G
-    if n < 1:
-        raise ValueError("n must be at least one")
-    it = iter(iterable)
-    while batch := tuple(islice(it, n)):
-        yield batch
+def next_category(src_ranges, maps: list[Map]):
+    dst_ranges = []
+    for start, start_range in src_ranges:
+        while start_range > 0:
+            for dst, src, size in maps:
+                if src <= start < src + size:
+                    size -= max(start - src, size - start_range)
+                    dst_ranges.append((start - src + dst, size))
+                    start += size
+                    start_range -= size
+                    break
+            else:
+                dst_ranges.append((start, start_range))
+                break
+    return dst_ranges
 
 
-def part2(s: str):
-    # if len(s) > 500:
-    #     return
-    chunks = paras(s)
-    seed_pairs = all_numbers(chunks.pop(0)[0])
-
-    fwd_map = {}
-    rev_map = {}
-    fwd_path = {}
-    rev_path = {}
-    for chunk in chunks:
-        [src, dst, *_] = re.split(r"-to-| ", chunk.pop(0))
-        fwd_map[src] = []
-        fwd_path[src] = dst
-        rev_map[dst] = []
-        rev_path[dst] = src
-        for line in chunk:
-            [dst_start, src_start, size] = all_numbers(line)
-            fwd_map[src].append((dst_start, src_start, size))
-            rev_map[dst].append((dst_start, src_start, size))
-        fwd_map[src].sort()
-        rev_map[dst].sort()
-
-    for i in count():
-        cur = "location"
-        nn = i
-        while cur != "seed":
-            maps = rev_map[cur]
-            nn = find_value_rev(nn, maps)
-            cur = rev_path[cur]
-
-        for start, size in batched(seed_pairs, 2):
-            if nn >= start and nn < start + size:
-                return i
+def lowest_location(
+    seeds: list[int], almanac: list[list[Map]], seed_fn: Callable
+) -> int:
+    return min(reduce(next_category, almanac, seed_fn(seeds)))[0]
 
 
 if __name__ == "__main__":
     main(
-        lambda s: part1(s),
-        lambda s: part2(s),
+        lambda s: lowest_location(*parse(s), lambda seeds: [(s, 1) for s in seeds]),
+        lambda s: lowest_location(*parse(s), lambda seeds: chunks(seeds, 2)),
     )
