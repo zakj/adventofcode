e90bb98a8b9d018d336fe14a0f2bdce131fbc4df
Zak Johnson <me@zakj.net>
2023-12-29 22:25:33 -0800

    py: 2023 day 23 cleanup/speedup
    * Use a simpler new DirectedGraph helper with slightly more sane API and
      data model.
    * Add a (currently unused) generic graph compress function.
    * Add a specific compress function to day 23.
    * Refactor dfs to use a stack instead of recursion.

--- py/2023/day23.py
+++ py/2023/day23.py
@@ -1,137 +1,86 @@
-# TODO: speed up, clean up
-from itertools import pairwise
-
 from aoc import main
-from coords import Dir, Point, addp
-from graph import Graph
+from coords import Dir, Point
+from coords import VVector as Vector
+from coords import addp
+from graph import DirectedGraph, Node
+
+SLOPES = {
+    "^": Dir.N,
+    ">": Dir.E,
+    "v": Dir.S,
+    "<": Dir.W,
+}
 
 
-def dfs(G, node, seen=None, path=None):
-    if seen is None:
-        seen = set()
-    if path is None:
-        path = [node]
-    seen.add(node)
-
-    paths = []
-    for neighbor in G.neighbors(node):
-        if neighbor not in seen:
-            next_path = path + [neighbor]
-            paths.append(tuple(next_path))
-            paths.extend(dfs(G, neighbor, seen.copy(), next_path))
-    return paths
-
-
-import sys
-
-# TODO noooo
-sys.setrecursionlimit(100000)
-
-
-def part1(s: str) -> int:
-    def edgeweights(from_node: Point, from_data, to_node: Point, to_data) -> bool:
-        fl, tl = from_data["label"], to_data["label"]
-        if fl == "^":
-            return addp(from_node, Dir.N) == to_node
-        if fl == ">":
-            return addp(from_node, Dir.E) == to_node
-        if fl == "v":
-            return addp(from_node, Dir.S) == to_node
-        if fl == "<":
-            return addp(from_node, Dir.W) == to_node
-        return fl != "#" and tl != "#"
-
-    def attrs(c: str) -> dict:
-        return {"label": c}
-
-    G = Graph.from_grid(s, edgeweights, attrs)
-    lines = s.splitlines()
-    start = (lines[0].index("."), 0)
-    end = (lines[-1].index("."), len(lines) - 1)
-
-    all_paths = dfs(G, start)
-    right_paths = [p for p in all_paths if p[-1] == end]
-    return max(len(p) for p in right_paths) - 1
+def dfs(G: DirectedGraph[Point], start: Point, end: Point):
+    seen = set()
+    queue: list[tuple[Point, int]] = [(start, 0)]
+    best = 0
+    while queue:
+        cur, distance = queue.pop()
+        if distance == -1:
+            seen.remove(cur)
+            continue
+        if cur == end:
+            best = max(best, distance)
+            continue
+        if cur in seen:
+            continue
+        seen.add(cur)
+        # Mark the node as visitable again after processing its children.
+        queue.append((cur, -1))
+        for neighbor in G.neighbors(cur):
+            queue.append((neighbor, distance + G.edges[cur, neighbor]))
+    return best
 
 
-from graphlib import TopologicalSorter
-
-
-def part2(s: str) -> int:
-    # if len(s) > 1000:
-    #     return -3
-
-    def edgeweights(from_node: Point, from_data, to_node: Point, to_data) -> bool:
-        fl, tl = from_data["label"], to_data["label"]
-        return fl != "#" and tl != "#"
-
-    def attrs(c: str) -> dict:
-        return {"label": c}
-
-    G = Graph.from_grid(s, edgeweights, attrs)
-    lines = s.splitlines()
-    start = (lines[0].index("."), 0)
-    end = (lines[-1].index("."), len(lines) - 1)
-
-    Gp: Graph[Point] = Graph()
-
-    def next_branch(start, end, seen) -> list[Point]:
+def compress(G: DirectedGraph[Point], start: Point) -> DirectedGraph[Point]:
+    def next_branch(start, seen) -> tuple[Point, int]:
         cur = start
-        path = [start]
+        distance = 0
         while True:
             seen.add(cur)
-            path.append(cur)
-            neighbors = set(G.neighbors(cur)) - seen
-            if cur == end or len(neighbors) > 1:
-                return path
-            if not neighbors:
-                return []
-            cur = list(neighbors)[0]
+            distance += 1
+            neighbors = G.neighbors(cur) - seen
+            if len(neighbors) != 1:
+                return cur, distance
+            cur = neighbors.pop()
 
-    # simplify graph
-    seen: set[Point] = set()
+    H = DirectedGraph()
     queue: list[Point] = [start]
+    seen = set()
     while queue:
         cur = queue.pop()
         if cur in seen:
             continue
         seen.add(cur)
         for neighbor in G.neighbors(cur):
-            if neighbor in seen:
-                continue
-            *path, branch = next_branch(neighbor, end, {cur})
-            seen |= set(path)
-            Gp.add_edge(cur, branch, weight=len(path))
-            Gp.add_edge(branch, cur, weight=len(path))
+            branch, weight = next_branch(neighbor, {cur})
+            H.add_edge(cur, branch, weight)
+            H.add_edge(branch, cur, weight)
             queue.append(branch)
+    return H
 
-    # TODO
-    # ts = TopologicalSorter()
-    # for u, vs in G.edges.items():
-    #     ts.add(u, tuple(vs.keys()))
-    # if len(s) > 1000:
-    #     return -2
-    # st = list(ts.static_order())
-    # dist = {}
-    # for node in st:
-    #     us = [(dist[u][0] + 1, u) for u in G.edges[node].items()]
-    #     maxu = max(us, key=lambda x: x[0]) if us else (0, node)
-    #     dist[node] = maxu if maxu[0] >= 0 else (0, node)
-    # print(f"{dist=}")
 
-    def weigh(G: Graph, path: list[Point]) -> int:
-        total = 0
-        for a, b in pairwise(path):
-            total += G.edges[a][b]["weight"]
-        return total
+def longest_path(s: str, slopes: dict[str, Vector] = {}) -> int:
+    def edgeweight(from_node: Point, from_data, to_node: Point, to_data) -> bool:
+        c = from_data["label"]
+        if c in slopes:
+            return addp(from_node, slopes[c]) == to_node
+        return c != "#" and to_data["label"] != "#"
 
-    all_paths = dfs(Gp, start)
-    right_paths = [weigh(Gp, p) for p in all_paths if p[-1] == end]
-    return max(p for p in right_paths)
+    G = DirectedGraph.from_grid(s, edgeweight)
+    lines = s.splitlines()
+    start = (lines[0].index("."), 0)
+    end = (lines[-1].index("."), len(lines) - 1)
+
+    if not slopes:
+        G = compress(G, start)
+    return dfs(G, start, end)
 
 
 if __name__ == "__main__":
     main(
-        lambda s: part1(s),
-        lambda s: part2(s),
+        lambda s: longest_path(s, SLOPES),
+        lambda s: longest_path(s),
     )
--- py/graph.py
+++ py/graph.py
@@ -9,6 +9,86 @@ Node = TypeVar("Node")
 GraphAttrs = dict[str, Any]
 
 
+class DirectedGraph(Generic[Node]):
+    def __init__(self) -> None:
+        self._adj: dict[Node, set[Node]] = defaultdict(set)
+        self._attrs: dict[Node, dict] = defaultdict(dict)
+        self._weight: dict[tuple[Node, Node], int] = {}
+
+    def __repr__(self) -> str:
+        return f"Graph with {len(self)} nodes, {len(list(self.edges))} edges"
+
+    def __contains__(self, node: Node) -> bool:
+        return node in self._adj
+
+    def __len__(self):
+        return len(self._adj)
+
+    @property
+    def nodes(self) -> dict[Node, dict]:
+        return self._attrs
+
+    @property
+    def edges(self) -> dict[tuple[Node, Node], int]:
+        return self._weight
+
+    def neighbors(self, node: Node) -> set[Node]:
+        return self._adj[node]
+
+    def add_node(self, node: Node, **kwargs) -> None:
+        self._adj[node]
+        self._attrs[node].update(**kwargs)
+
+    def add_edge(self, a: Node, b: Node, weight: int = 1) -> None:
+        self.add_node(a)
+        self.add_node(b)
+        self._adj[a].add(b)
+        self._weight[a, b] = weight
+
+    def remove_node(self, node: Node) -> None:
+        edges = [(u, v) for (u, v) in self.edges if node in (u, v)]
+        for u, v in edges:
+            self.remove_edge(u, v)
+        del self._adj[node]
+        del self._attrs[node]
+
+    def remove_edge(self, a: Node, b: Node) -> None:
+        self._adj[a].remove(b)
+        del self._weight[a, b]
+
+    @classmethod
+    def from_grid(
+        cls,
+        s: str,
+        edgeweightfn: Callable[[Point, dict, Point, dict], bool | int],
+        attrsfn: Callable[[str], dict] = lambda c: {"label": c},
+    ) -> "DirectedGraph[Point]":
+        lines = s.splitlines()
+        cols = range(len(lines[0]))
+        rows = range(len(lines))
+
+        G = DirectedGraph()
+        for y in rows:
+            for x in cols:
+                G.add_node((x, y), **attrsfn(lines[y][x]))
+
+        adjacents = [((x, y), (px, y)) for y in rows for px, x in pairwise(cols)]
+        adjacents += [((x, y), (x, py)) for py, y in pairwise(rows) for x in cols]
+        adjacents += [(b, a) for a, b in adjacents]
+
+        for a, b in adjacents:
+            r = edgeweightfn(a, G.nodes[a], b, G.nodes[b])
+            match r:
+                case True:
+                    G.add_edge(a, b, weight=1)
+                case False:
+                    pass
+                case int(weight):
+                    G.add_edge(a, b, weight=weight)
+
+        return G
+
+
 class Graph(Generic[Node]):
     nodes: dict[Node, GraphAttrs]
     edges: dict[Node, dict[Node, GraphAttrs]]
@@ -69,6 +149,22 @@ class Graph(Generic[Node]):
                     G.add_edge(a, b, weight=weight)
 
         return G
+
+
+def compress(G: DirectedGraph) -> None:
+    """Remove nodes between exactly two other nodes. Slow, but generic."""
+    for node in list(G.nodes):
+        neighbors = G.neighbors(node)
+        if len(neighbors) != 2:
+            continue
+        a, b = neighbors
+        # a and b must not already be connected, and a<->node<->b must be two-way.
+        assert b not in G.neighbors(a) and a not in G.neighbors(b)
+        assert node in G.neighbors(a) and node in G.neighbors(b)
+        weight = G.edges[a, node] + G.edges[node, b]
+        G.add_edge(a, b, weight)
+        G.add_edge(b, a, weight)
+        G.remove_node(node)
 
 
 def shortest_path(G: Graph[Node], start: Node, end: Node) -> list[Node]:
