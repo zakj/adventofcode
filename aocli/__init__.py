2ff922cfc4960e3dc82958210f9fb41ed7d13918
Zak Johnson <me@zakj.net>
2024-01-02 21:42:32 -0800

    aocli: update documentation

--- aocli/__init__.py
+++ aocli/__init__.py
@@ -1,21 +1,38 @@
 """
-Each "day" program should expect to be called with a single argument: the path
-to a named pipe. On stdin, it should expect a series of inputs, each separated
-by the ASCII file separator (0x1c). Each input contains is composed of two
-parts: the input data itself, and an optional JSON object with required keyword
-arguments for that input. The input data and JSON are separated by an ASCII
-record separator (0x1e).
+Each day's solver should expect to be called with a single command-line
+argument: the URL to a websocket server. It should connect to that solver and
+expect to receive one or more Input messages, followed by a Done message. For
+each Input message, it should run its solvers on the input. For each solver, it
+should emit a Results message.
 
-It should run its solvers on the input and write one message per solver to the
-named pipe. Each message is a JSON object with a "results" key (of any type)
-and a "duration" float (in seconds). Messages are delimited with a newline
-character. The JSON object may optionally also contain an "aside" key, itself
-an object with a "header" key (a list of strings), and a "rows" key (a list of
-lists of strings). This will be displayed alongside the results.
+When it has run all solvers on a given input, it should send a Done message to
+indicate it is ready for another input, if any. When the server has exhausted
+its inputs, it will send its own Done message, and the solver can safely exit.
 
-TODO: Pull this out of the adventofcode repo into its own, and install it
-separately. Figure out how to configure RUNNERS separately as well.
+Message formats:
+
+    Input {
+        "input": <string: Advent of Code input data for the day>,
+        "args": <object?: key/value pairs for arguments to the solver>,
+        "part": <int?: if the given input is relevant only for one part>,
+    }
+
+    Results {
+        "answer": <any: the answer for that solver/input>,
+        "duration": <float: time in seconds spent solving>,
+        "aside": <Aside?: tabular profiling data>,
+    }
+
+    Aside {
+        "header": <list of str: column names>,
+        "rows": <list of list of str: cells>,
+    }
+
+    Done { "done": true }
 """
+
+# TODO: Pull this out of the adventofcode repo into its own, and install it
+# separately. Figure out how to configure RUNNERS separately as well.
 
 from pathlib import Path
 
