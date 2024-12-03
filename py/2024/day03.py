bde8ec39f2c0a0ce95c355598df5fab91d3c8a6f
Zak Johnson <me@zakj.net>
2024-12-02 21:56:29 -0800

    24d03 minor cleanup

--- py/2024/day03.py
+++ py/2024/day03.py
@@ -1,55 +1,54 @@
 import re
+from dataclasses import dataclass
+from enum import Enum
 
 from aoc import main
 
 
-def parse(s: str):
-    pass
+class Op(Enum):
+    Mul = 1
+    Do = 2
+    Dont = 3
 
 
-def part1(s: str) -> int:
-    mul_re = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
-    matches = mul_re.findall(s)
+@dataclass
+class Statement:
+    start: int
+    op: Op
+    value: int = 0
+
+
+mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+
+
+def multiply(s: str) -> int:
+    matches = re.findall(mul_pattern, s)
+    return sum(int(m[0]) * int(m[1]) for m in matches)
+
+
+def multiply_with_enables(s: str) -> int:
+    statements = [
+        Statement(m.start(), Op.Mul, int(m.group(1)) * int(m.group(2)))
+        for m in re.finditer(mul_pattern, s)
+    ]
+    statements.extend([Statement(m.start(), Op.Do) for m in re.finditer(r"do\(\)", s)])
+    statements.extend(
+        [Statement(m.start(), Op.Dont) for m in re.finditer(r"don't\(\)", s)]
+    )
+    statements.sort(key=lambda m: m.start)
+
     total = 0
-    for match in matches:
-        total += int(match[0]) * int(match[1])
-    return total
-
-
-def part2(s: str) -> int:
-    mul_re = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
-    do_re = re.compile(r"do\(\)")
-    dont_re = re.compile(r"don't\(\)")
-    muls = mul_re.finditer(s)
-    dos = do_re.finditer(s)
-    donts = dont_re.finditer(s)
-
-    indexes: dict[int, re.Match[str]] = {}
-    for m in muls:
-        indexes[m.start()] = m
-    for m in dos:
-        indexes[m.start()] = m
-    for m in donts:
-        indexes[m.start()] = m
-
     enabled = True
-    total = 0
-    for k in sorted(indexes.keys()):
-        m = indexes[k]
-        if m.group().startswith("do("):
-            print("found a do")
+    for st in statements:
+        if st.op == Op.Do:
             enabled = True
-        elif m.group().startswith("don"):
-            print("found a dont")
+        elif st.op == Op.Dont:
             enabled = False
-        elif m.group().startswith("mul") and enabled:
-            total += int(m.group(1)) * int(m.group(2))
+        elif st.op == Op.Mul and enabled:
+            total += st.value
 
     return total
 
 
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(multiply, multiply_with_enables)
