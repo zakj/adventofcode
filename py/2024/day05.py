83dcaf9861362d0d1a77d35466eb28b6c2e5739c
Zak Johnson <me@zakj.net>
2024-12-04 22:08:54 -0800

    24d05 cleanup

--- py/2024/day05.py
+++ py/2024/day05.py
@@ -1,82 +1,37 @@
+from collections.abc import Callable
+from functools import cmp_to_key
+
 from aoc import main
 from parse import all_numbers, paras
 
 
-def parse(s: str) -> tuple[list[tuple[int, int]], list[list[int]]]:
+def parse(s: str) -> tuple[Callable, list[list[int]]]:
     rules_str, pages_str = paras(s)
-    rules = []
-    for line in rules_str:
-        a, b = all_numbers(line)
-        rules.append((a, b))
-    pages = []
-    for line in pages_str:
-        pages.append(all_numbers(line))
-    return rules, pages
+    rules = {tuple(all_numbers(line)) for line in rules_str}
+    pages = [all_numbers(line) for line in pages_str]
+
+    @cmp_to_key
+    def compare(a: int, b: int) -> int:
+        if (a, b) in rules:
+            return -1
+        if (b, a) in rules:
+            return 1
+        return 0
+
+    return compare, pages
 
 
-def is_valid(pages, rules):
-    for i in range(len(pages)):
-        p = pages[i]
-        afters = {b for a, b in rules if a == p}
-        for j in range(i):
-            if pages[j] in afters:
-                return False
-        befores = {a for a, b in rules if b == p}
-        for j in range(i, len(pages)):
-            if pages[j] in befores:
-                return False
-    return True
+def sum_valid_middle_pages(s: str) -> int:
+    key, updates = parse(s)
+    updates = [(pages, sorted(pages, key=key)) for pages in updates]
+    return sum([a[len(a) // 2] for a, b in updates if a == b])
 
 
-def invalid_at(pages, rules) -> tuple[int, int]:
-    for i in range(len(pages)):
-        p = pages[i]
-        afters = {b for a, b in rules if a == p}
-        for j in range(i):
-            if pages[j] in afters:
-                return i, j
-        befores = {a for a, b in rules if b == p}
-        for j in range(i, len(pages)):
-            if pages[j] in befores:
-                return i, j
-    raise
-
-
-def part1(s: str) -> int:
-    rules, updates = parse(s)
-    valid = []
-    for pages in updates:
-        if is_valid(pages, rules):
-            valid.append(pages)
-
-    total = 0
-    for pages in valid:
-        total += pages[len(pages) // 2]
-
-    return total
-
-
-def part2(s: str) -> int:
-    rules, updates = parse(s)
-    invalid = []
-    for pages in updates:
-        if not is_valid(pages, rules):
-            invalid.append(pages)
-
-    for pages in invalid:
-        while not is_valid(pages, rules):
-            i, j = invalid_at(pages, rules)
-            pages[i], pages[j] = pages[j], pages[i]
-
-    total = 0
-    for pages in invalid:
-        total += pages[len(pages) // 2]
-
-    return total
+def sum_corrected_invalid_pages(s: str) -> int:
+    key, updates = parse(s)
+    updates = [(pages, sorted(pages, key=key)) for pages in updates]
+    return sum([b[len(b) // 2] for a, b in updates if a != b])
 
 
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(sum_valid_middle_pages, sum_corrected_invalid_pages)
