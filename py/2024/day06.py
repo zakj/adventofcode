ee5e78848ae938d3e1e4d93414fe6d7e47980198
Zak Johnson <me@zakj.net>
2024-12-06 14:51:43 -0800

    24d06 optimization

--- py/2024/day06.py
+++ py/2024/day06.py
@@ -1,44 +1,57 @@
+from collections import defaultdict
+
 from aoc import main
 from coords import Dir, Grid, Point, Vector, addp, subp, turn_right
 
 State = tuple[Point, Vector]
+Visits = dict[Point, list[Vector]]
 
 
-def parse(s: str) -> tuple[Grid, State]:
+class FoundLoop(Exception):
+    pass
+
+
+def parse(s: str) -> tuple[Grid[str], State]:
     grid = Grid(s)
     return grid, (grid.findall("^")[0], Dir.N)
 
 
-def walk(grid: Grid, start: State) -> tuple[bool, set[tuple[Point, Vector]]]:
-    seen = set()
+def walk(grid: Grid, start: State) -> Visits:
+    visits: Visits = defaultdict(list)
     cur, dir = start
     while cur in grid:
-        if (cur, dir) in seen:
-            return True, seen
-        seen.add((cur, dir))
+        if dir in visits[cur]:
+            raise FoundLoop
+        visits[cur].append(dir)
         cur = addp(cur, dir)
         while grid.get(cur) == "#":
             cur = subp(cur, dir)
             dir = turn_right(dir)
-    return False, seen
+    return visits
 
 
 def visited_positions(s: str) -> int:
     grid, start = parse(s)
-    _, seen = walk(grid, start)
-    return len({p for p, d in seen})
+    visits = walk(grid, start)
+    return len(visits)
 
 
 def count_looping_obstacles(s: str) -> int:
     grid, start = parse(s)
-    _, seen = walk(grid, start)
-    obstacle_options = {p for p, d in seen} - {start[0]}
+    visits = walk(grid, start)
     count = 0
-    for point in obstacle_options:
-        grid[point] = "#"
-        if walk(grid, start)[0]:
+    for obstacle in set(visits.keys()) - {start[0]}:
+        grid[obstacle] = "#"
+        # Optimization: start the walk from the point/dir at which we would have
+        # first encountered this obstacle, to save us repeating steps walking
+        # from the original start.
+        dir = visits[obstacle][0]
+        start = subp(obstacle, dir)
+        try:
+            walk(grid, (start, dir))
+        except FoundLoop:
             count += 1
-        grid[point] = "."
+        grid[obstacle] = "."
     return count
 
 
