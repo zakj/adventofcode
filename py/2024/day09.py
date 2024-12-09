df3aee17b37eeb5206a20a984340905419680551
Zak Johnson <me@zakj.net>
2024-12-09 09:54:37 -0800

    24d09 cleanup

--- py/2024/day09.py
+++ py/2024/day09.py
@@ -1,39 +1,7 @@
 from dataclasses import dataclass
+from itertools import count
 
 from aoc import main
-
-
-def parse(s: str):
-    pass
-
-
-def part1(s: str) -> int:
-    id = 0
-    block = True
-    fs = []
-    for c in s.strip():
-        size = int(c)
-        if block:
-            fs.extend([str(id)] * size)
-            id += 1
-        else:
-            fs.extend("." * size)
-        block = not block
-    last_free_idx = 0
-    for i in range(len(fs) - 1, 0, -1):
-        c = fs[i]
-        first_free_idx = fs.index(".", last_free_idx)
-        last_free_idx = first_free_idx
-        if first_free_idx >= i:
-            break
-        fs[i], fs[first_free_idx] = fs[first_free_idx], fs[i]
-
-    checksum = 0
-    for i, c in enumerate(fs):
-        if c != ".":
-            checksum += i * int(c)
-
-    return checksum
 
 
 @dataclass
@@ -44,26 +12,44 @@ class Block:
     def __str__(self) -> str:
         return "".join(["." if self.id is None else str(self.id)] * self.size)
 
+    def expand(self) -> list[int | None]:
+        return [self.id] * self.size
 
-def part2(s: str) -> int:
-    id = 0
-    block = True
-    fs: list[Block] = []
-    for c in s.strip():
-        size = int(c)
-        if block:
-            fs.append(Block(id, size))
-            id += 1
-        else:
-            fs.append(Block(None, size))
-        block = not block
 
+def parse(s: str) -> list[Block]:
+    id = count()
+    return [
+        Block(next(id) if i % 2 == 0 else None, int(c)) for i, c in enumerate(s.strip())
+    ]
+
+
+def checksum(expanded: list[int | None]) -> int:
+    return sum(i * c if c is not None else 0 for i, c in enumerate(expanded))
+
+
+def compress_disk(s: str) -> int:
+    # Since we're splitting up files, it's simpler to just work on expanded data.
+    fs = [c for block in parse(s) for c in block.expand()]
+    first_free_idx = 0
+    for i in range(len(fs) - 1, 0, -1):
+        if fs[i] is None:
+            continue
+        try:
+            first_free_idx = fs.index(None, first_free_idx, i)
+        except ValueError:
+            break
+        fs[i], fs[first_free_idx] = fs[first_free_idx], fs[i]
+    return checksum(fs)
+
+
+def defrag_disk(s: str) -> int:
     # Conceptually, this should not use a range: we are adding to the array
     # during iteration. I previously was backtracking each iteration to find the
     # next-untried file. But this is a lot faster, and because the input
     # alternates between files and free space, we are inserting only one item
     # into the array, and we only care about files here, it's safe in this case:
     # we only ever skip processing a free block.
+    fs = parse(s)
     for block_idx in range(len(fs) - 1, 0, -1):
         block = fs[block_idx]
         if block.id is None:
@@ -86,19 +72,9 @@ def part2(s: str) -> int:
         if extra_space > 0:
             fs.insert(first_free_idx + 1, Block(None, extra_space))
 
-    checksum = 0
-    i = 0
-    for block in fs:
-        if block.id:
-            for j in range(i, i + block.size):
-                checksum += j * block.id
-        i += block.size
-
-    return checksum
+    expanded = [c for block in fs for c in block.expand()]
+    return checksum(expanded)
 
 
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(compress_disk, defrag_disk)
