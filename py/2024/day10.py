5071fa381a66829161a53006ce0abddeac0a836d
Zak Johnson <me@zakj.net>
2024-12-09 21:49:25 -0800

    24d10 quick cleanup

--- py/2024/day10.py
+++ py/2024/day10.py
@@ -1,50 +1,32 @@
-from aoc import main, status
+from aoc import main
 from coords import Point
-from graph import (
-    GridGraph,
-    all_shortest_path_lengths,
-    shortest_path_length,
-    shortest_path_lengths_from,
-)
+from graph import GridGraph, shortest_path_lengths_from
 
 
-def parse(s: str):
-    pass
-
-
-def part1(s: str) -> int:
+def parse(s: str) -> tuple[GridGraph, list[Point]]:
     def edges(pa: Point, ca: str, pb: Point, cb: str) -> bool:
         return int(ca) + 1 == int(cb)
 
     G = GridGraph(s, edges)
+    return G, [p for p, c in G.type.items() if c == "0"]
 
-    trailheads = [p for p, c in G.type.items() if c == "0"]
+
+def trail_score(s: str) -> int:
+    G, trailheads = parse(s)
     score = 0
-    for i, start in enumerate(trailheads):
-        score += len(
-            [
-                1
-                for point, distance in shortest_path_lengths_from(G, start)
-                if G.type[point] == "9"
-            ]
-        )
+    for start in trailheads:
+        score += len([1 for _, d in shortest_path_lengths_from(G, start) if d == 9])
     return score
 
 
-def part2(s: str) -> int:
-    def edges(pa: Point, ca: str, pb: Point, cb: str) -> bool:
-        return int(ca) + 1 == int(cb)
-
-    G = GridGraph(s, edges)
-
-    trailheads = [p for p, c in G.type.items() if c == "0"]
+def trail_rating(s: str) -> int:
+    G, trailheads = parse(s)
     trailends = [p for p, c in G.type.items() if c == "9"]
-    score = 0
-    for i, start in enumerate(trailheads):
-        status(f"{i + 1} / {len(trailheads)}")
+    rating = 0
+    for start in trailheads:
         for end in trailends:
-            score += len(list(all_paths(G, start, end, set())))
-    return score
+            rating += len(list(all_paths(G, start, end, set())))
+    return rating
 
 
 def all_paths(G, start: Point, end: Point, visited: set[Point]):
@@ -58,7 +40,4 @@ def all_paths(G, start: Point, end: Point, visited: set[Point]):
 
 
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(trail_score, trail_rating)
