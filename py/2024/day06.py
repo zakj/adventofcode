b755ef36dd865c9d2baea97f6c030b44c168d3ae
Zak Johnson <me@zakj.net>
2024-12-11 16:24:32 -0800

    24d06 optimization
    Walking the grid step by step is slow. Optimize by storing obstacles in
    two dictionaries of lists. obstacles_by_x has x-coordinate keys, and
    its values are lists of y-coordinates of obstacles corresponding to the
    key. Then we can quickly bisect to find the neighboring obstacles on
    the appropriate axis. Runtime from ~2.6s to ~130ms.

--- py/2024/day06.py
+++ py/2024/day06.py
@@ -1,3 +1,4 @@
+from bisect import bisect, insort
 from collections import defaultdict
 
 from aoc import main
@@ -5,6 +6,7 @@ from coords import Dir, Grid, Point, Vector, addp, subp, turn_right
 
 State = tuple[Point, Vector]
 Visits = dict[Point, list[Vector]]
+Obstacles = dict[int, list[int]]
 
 
 class FoundLoop(Exception):
@@ -36,26 +38,54 @@ def visited_positions(s: str) -> int:
     return len(visits)
 
 
-# TODO: this is still slow. Could we cache the straight walks from the
-# unmodified grid, and use that instead of walking cell by cell? We'd have to
-# account for when a new obstacle appears.
 def count_looping_obstacles(s: str) -> int:
     grid, start = parse(s)
+    obs_by_x = defaultdict[int, list[int]](list)
+    obs_by_y = defaultdict[int, list[int]](list)
+    for x, y in grid.findall("#"):
+        obs_by_x[x].append(y)
+        obs_by_y[y].append(x)
+
+    # Optimization: start the walk from the point/dir at which we would have
+    # first encountered this obstacle, to save us repeating steps walking
+    # from the original start.
     visits = walk(grid, start)
     count = 0
-    for obstacle in set(visits.keys()) - {start[0]}:
-        grid[obstacle] = "#"
-        # Optimization: start the walk from the point/dir at which we would have
-        # first encountered this obstacle, to save us repeating steps walking
-        # from the original start.
-        dir = visits[obstacle][0]
-        start = subp(obstacle, dir)
-        try:
-            walk(grid, (start, dir))
-        except FoundLoop:
+    for x, y in set(visits.keys()) - {start[0]}:
+        insort(obs_by_x[x], y)
+        insort(obs_by_y[y], x)
+        dir = visits[x, y][0]
+        start = subp((x, y), dir)
+        if has_loop(grid, (start, turn_right(dir)), obs_by_x, obs_by_y):
             count += 1
-        grid[obstacle] = "."
+        obs_by_x[x].remove(y)
+        obs_by_y[y].remove(x)
     return count
+
+
+# Optimization: rather than walking through the grid step by step, quickly find
+# the next obstacle in the current direction and hop to it.
+def has_loop(
+    grid: Grid, start: State, obs_by_x: Obstacles, obs_by_y: Obstacles
+) -> bool:
+    visits: Visits = defaultdict(list)
+    (x, y), dir = start
+    while (x, y) in grid:
+        if dir in visits[x, y]:
+            return True
+        visits[x, y].append(dir)
+
+        horizontal = dir in {Dir.E, Dir.W}
+        obstacles = obs_by_y[y] if horizontal else obs_by_x[x]
+        value = x if horizontal else y
+        idx = bisect(obstacles, value) - (1 if dir in {Dir.W, Dir.N} else 0)
+        if not 0 <= idx < len(obstacles):
+            break
+        obs = (obstacles[idx], y) if horizontal else (x, obstacles[idx])
+
+        x, y = subp(obs, dir)
+        dir = turn_right(dir)
+    return False
 
 
 if __name__ == "__main__":
