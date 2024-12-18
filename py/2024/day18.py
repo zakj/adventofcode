d7d2cb6ffbb645f48e2ce63de976b6a1db82f8cf
Zak Johnson <me@zakj.net>
2024-12-17 22:05:00 -0800

    24d18 clean up and optimize
    Hack solution for part 2 was 27s, using DiGraph.remove_node instead of
    re-creating the graph each time got me to 2.4s, binary search got me
    down to 125ms.

--- py/2024/day18.py
+++ py/2024/day18.py
@@ -1,80 +1,46 @@
-from itertools import count
+from bisect import bisect_left
 
 from aoc import main
-from coords import Grid, Point, neighbors
-from graph import GridGraph, shortest_path_length
-from parse import all_numbers
+from coords import Point
+from graph import DiGraph, GridGraph, shortest_path_length
+from parse import all_numbers, line_parser
 
 
-class Grid2(Grid):
-    def display2(self) -> str:
-        rows = []
-        for y in range(self.height):
-            rows.append("".join(str(self[x, y]) for x in range(self.width)))
-        return "\n".join(rows)
+@line_parser
+def parse(line: str) -> Point:
+    x, y = all_numbers(line)
+    return x, y
 
 
-def parse(s: str, size: int):
+def make_graph(size: int, bytes: list[Point]) -> DiGraph[Point]:
     row = "." * size
-    rows = [row] * size
-    grid = Grid2("\n".join(rows))
-    bytes = []
-    for line in s.splitlines():
-        x, y = all_numbers(line)
-        bytes.append((x, y))
-    return grid, bytes
-
-
-def part1(s: str, size: int, first: int) -> int:
-    grid, bytes = parse(s, size)
-    bytes = bytes[:first]
-    start = (0, 0)
-    positions = {start}
-    queue = [(start, 0)]
-    goal = (size - 1, size - 1)
-    for byte in bytes:
-        grid[byte] = "#"
+    rows = "\n".join([row] * size)
+    bytes_set = set(bytes)
 
     def edges(a: Point, ac: str, b: Point, bc: str) -> bool:
-        return bc != "#"
+        return b not in bytes_set
 
-    G = GridGraph(grid.display2(), edges)
+    return GridGraph(rows, edges)
+
+
+def minimum_steps(s: str, size: int, first: int) -> int:
+    bytes = parse(s)
+    start, goal = (0, 0), (size - 1, size - 1)
+    G = make_graph(size, bytes[:first])
     return shortest_path_length(G, start, goal)
-    for t in count():
-        newpos = set()
-        for cur in positions:
-            if cur == goal:
-                return t
-            for neighbor in neighbors(cur):
-                if neighbor in grid and grid[neighbor] != "#":
-                    newpos.add(neighbor)
-        positions = newpos
-    # not 138
-    return 0
 
 
-def part2(s: str, size: int, first: int) -> str:
-    grid, bytes = parse(s, size)
-    start = (0, 0)
-    goal = (size - 1, size - 1)
-    for byte in bytes[:first]:
-        grid[byte] = "#"
+def first_cutoff_byte(s: str, size: int, first: int) -> str:
+    bytes = parse(s)
+    start, goal = (0, 0), (size - 1, size - 1)
 
-    def edges(a: Point, ac: str, b: Point, bc: str) -> bool:
-        return bc != "#"
+    def bisect_key(i: int) -> bool:
+        G = make_graph(size, bytes[: i + 1])
+        return shortest_path_length(G, start, goal) == -1
 
-    G = GridGraph(grid.display2(), edges)
-
-    for t in bytes[first:]:
-        G.remove_node(t)
-        if shortest_path_length(G, start, goal) == -1:
-            return f"{t[0]},{t[1]}"
-
-    raise ValueError
+    i = bisect_left(list(range(len(bytes))), True, lo=first, key=bisect_key)
+    return f"{bytes[i][0]},{bytes[i][1]}"
 
 
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(minimum_steps, first_cutoff_byte)
