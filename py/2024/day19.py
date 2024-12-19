00ad8aa6d5a4276994e4c6f3021bce85bd5e9865
Zak Johnson <me@zakj.net>
2024-12-18 22:00:03 -0800

    24d19 cleanup

--- py/2024/day19.py
+++ py/2024/day19.py
@@ -1,40 +1,36 @@
 from collections import defaultdict
+from functools import cache
 
-from aoc import main, status
+from aoc import main
 from parse import paras
+
+# global because we don't want it in the caches
+towels = {}
 
 
 def parse(s: str):
-    pass
-
-
-from functools import cache
-
-towels_dict = {}
+    towels_str, patterns = paras(s)
+    global towels
+    towels = defaultdict(list)
+    for towel in towels_str[0].split(", "):
+        towels[towel[0]].append(towel)
+    return patterns
 
 
 @cache
 def is_valid(pattern) -> bool:
     if not pattern:
         return True
-    for towel in towels_dict[pattern[0]]:
+    for towel in towels[pattern[0]]:
         if len(towel) <= len(pattern) and pattern.startswith(towel):
             if is_valid(pattern[len(towel) :]):
                 return True
     return False
 
 
-def part1(s: str) -> int:
-    global towels_dict
-    lines = s.splitlines()
-    towels = lines[0].split(", ")
-    towels.sort(key=lambda t: len(t))
-    towels.reverse()
-    towels_dict = defaultdict(list)
+def count_valid_patterns(s: str) -> int:
+    patterns = parse(s)
     is_valid.cache_clear()
-    for towel in towels:
-        towels_dict[towel[0]].append(towel)
-    patterns = lines[2:]
     return len([1 for p in patterns if is_valid(p)])
 
 
@@ -42,33 +38,18 @@ def part1(s: str) -> int:
 def towel_options(pattern) -> int:
     if not pattern:
         return 1
-
     count = 0
-    for towel in towels_dict[pattern[0]]:
+    for towel in towels[pattern[0]]:
         if len(towel) <= len(pattern) and pattern.startswith(towel):
-            sub = pattern[len(towel) :]
-            if is_valid(sub):
-                count += towel_options(sub)
+            count += towel_options(pattern[len(towel) :])
     return count
 
 
-def part2(s: str) -> int:
-    global towels_dict
-    lines = s.splitlines()
-    towels = lines[0].split(", ")
-    towels.sort(key=lambda t: len(t))
-    towels.reverse()
-    towels_dict = defaultdict(list)
-    is_valid.cache_clear()
+def count_configurations(s: str) -> int:
+    patterns = parse(s)
     towel_options.cache_clear()
-    for towel in towels:
-        towels_dict[towel[0]].append(towel)
-    patterns = lines[2:]
     return sum(towel_options(p) for p in patterns)
 
 
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(count_valid_patterns, count_configurations)
