cbc0944d32eada85aff4b24552fdd1fb67af7e74
Zak Johnson <me@zakj.net>
2024-12-22 09:05:08 -0800

    24d22 tidying

--- py/2024/day22.py
+++ py/2024/day22.py
@@ -1,57 +1,41 @@
 from collections import defaultdict
+from itertools import pairwise
 
 from aoc import main, progress
 from parse import all_numbers
 from util import sliding_window
 
-PRUNE = 16777216
-
 
 def next_secret(n: int) -> int:
-    n ^= n * 64
-    n %= PRUNE
-    n ^= n // 32
-    n %= PRUNE
-    n ^= n * 2048
-    n %= PRUNE
-    return n
+    n = (n ^ (n * 64)) % 0x1000000
+    n = n ^ (n // 32)  # prune here is a no-op
+    return (n ^ (n * 2048)) % 0x1000000
 
 
-def part1(s: str) -> int:
-    initial_secrets = all_numbers(s)
+def sum_of_secrets(s: str) -> int:
     total = 0
-    for n in initial_secrets:
+    for n in all_numbers(s):
         for i in range(2000):
             n = next_secret(n)
         total += n
     return total
 
 
-def part2(s: str) -> int:
-    initial_secrets = all_numbers(s)
-
+def best_sequence_of_changes(s: str) -> int:
     running_total = defaultdict(int)
-    for n in progress(initial_secrets):
+    for n in progress(all_numbers(s)):
         values = [n % 10]
-        changes: list[int] = []
         for i in range(2000):
             n = next_secret(n)
-            d = n % 10
-            changes.append(d - values[-1])
             values.append(n % 10)
-
+        changes = [a - b for a, b in pairwise(values)]
         seen = set()
         for i, sequence in enumerate(sliding_window(changes, 4)):
             if sequence not in seen:
                 running_total[sequence] += values[i + 4]
                 seen.add(sequence)
-
     return max(running_total.values())
 
 
-# TODO: cleanup
 if __name__ == "__main__":
-    main(
-        part1,
-        part2,
-    )
+    main(sum_of_secrets, best_sequence_of_changes)
