f0ad4ebafe23165d1f79c087d916d54fa9e38844
Zak Johnson <me@zakj.net>
2024-12-22 22:31:37 -0800

    24d23 another cleanup

--- py/2024/day23.py
+++ py/2024/day23.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 from itertools import combinations
 
+from typing_extensions import Mapping
+
 from aoc import main
 
+type Edges = Mapping[str, set[str]]
 
-def parse(s: str):
+
+def parse(s: str) -> Edges:
     edges = defaultdict(set)
     for line in s.splitlines():
         a, b = line.split("-")
@@ -18,23 +22,17 @@ def part1(s: str) -> int:
     regions = set()
     for node, neighbors in edges.items():
         for a, b in combinations(neighbors, 2):
-            if b in edges[a] and a in edges[b]:
+            if a in edges[b] and any(n.startswith("t") for n in [node, a, b]):
                 regions.add(frozenset([node, a, b]))
-    count = 0
-    for region in regions:
-        if any(node.startswith("t") for node in region):
-            count += 1
-    return count
+    return len(regions)
 
 
-def best_region(edges, nodes: set[str]):
-    xs = list(nodes)
+def best_region(edges: Edges, nodes: set[str]):
     connected_count = defaultdict(int)
-    for i, a in enumerate(xs):
-        for b in xs[i + 1 :]:
-            if a in edges[b]:
-                connected_count[a] += 1
-                connected_count[b] += 1
+    for a, b in combinations(nodes, 2):
+        if a in edges[b]:
+            connected_count[a] += 1
+            connected_count[b] += 1
     x = max(connected_count.values())
     best = {n for n, c in connected_count.items() if c == x}
     return best if len(best) == x + 1 else set()
