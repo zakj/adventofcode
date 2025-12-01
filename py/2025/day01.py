7cb5db266332e40f9355c70cb2fcafd234f30c95
Zak Johnson <me@zakj.net>
2025-12-01 12:51:07 -0800

    25d1 more tidying

--- py/2025/day01.py
+++ py/2025/day01.py
@@ -1,42 +1,32 @@
+from collections.abc import Iterable
+from itertools import accumulate
 from typing import Literal
 
 from aoc import main
 from aoc.parse import first_number, line_parser
 
-START = 50
-DIAL_SIZE = 100
-
-type Turn = tuple[Literal[1] | Literal[-1], int]
+type Rotation = tuple[Literal[1] | Literal[-1], int]
 
 
 @line_parser
-def parse(line: str) -> Turn:
-    return (-1 if line[0] == "L" else 1, first_number(line))
+def parse(line: str) -> Rotation:
+    return (1 if line[0] == "R" else -1, first_number(line))
 
 
-def zero_counts(turns: list[Turn]) -> int:
-    cur = START
-    count = 0
-    for dir, length in turns:
-        cur = (cur + length * dir) % DIAL_SIZE
-        if cur == 0:
-            count += 1
-    return count
+def count_zeroes(states: Iterable[int], start=50, size=100) -> int:
+    return sum(1 for x in accumulate(states, initial=start) if x % size == 0)
 
 
-def zero_counts_with_pass(turns: list[Turn]):
-    cur = START
-    count = 0
-    for dir, length in turns:
-        for _ in range(length):
-            cur = (cur + dir) % DIAL_SIZE
-            if cur == 0:
-                count += 1
-    return count
+def turns(rotations: Iterable[Rotation]) -> Iterable[int]:
+    return [delta * distance for delta, distance in rotations]
+
+
+def clicks(rotations: Iterable[Rotation]) -> Iterable[int]:
+    return [delta for delta, distance in rotations for _ in range(distance)]
 
 
 if __name__ == "__main__":
     main(
-        lambda s: zero_counts(parse(s)),
-        lambda s: zero_counts_with_pass(parse(s)),
+        lambda s: count_zeroes(turns(parse(s))),
+        lambda s: count_zeroes(clicks(parse(s))),
     )
