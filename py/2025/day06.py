9bec8d62fd66b00ba8176c863f134e6c6c8d5d02
Zak Johnson <me@zakj.net>
2025-12-05 21:44:25 -0800

    25d6 quick cleanup

--- py/2025/day06.py
+++ py/2025/day06.py
@@ -1,51 +1,40 @@
 import re
+from itertools import pairwise
 from math import prod
 
 from aoc import main
 
+OPERATORS = {"+": sum, "*": prod}
+
 
 def part1(input: str):
     lines = [re.split(r"\s+", line.strip()) for line in input.splitlines()]
-    grand_total = 0
+    total = 0
     for x in range(len(lines[0])):
         problem = []
         for y in range(len(lines)):
             problem.append(lines[y][x])
         op = problem.pop()
-        if op == "*":
-            grand_total += prod(int(n) for n in problem)
-        elif op == "+":
-            grand_total += sum(int(n) for n in problem)
-    return grand_total
+        total += OPERATORS[op](int(n) for n in problem)
+    return total
 
 
 def part2(input: str):
     lines = input.splitlines()
     operators = lines.pop()
-    col_widths = []
-    width = 0
+    cols = []
     for i, c in enumerate(operators):
-        if c == " ":
-            width += 1
-        elif width:
-            col_widths.append(width)
-            width = 0
-    col_widths.append(width + 1)
+        if c != " ":
+            cols.append(i)
+    cols.append(len(operators) + 1)
 
     total = 0
-    start = 0
-    for i, width in enumerate(col_widths):
+    for start, next_col in pairwise(cols):
         op = operators[start]
-        nums = []
-        for line in lines:
-            nums.append(line[start : start + width])
-        start = start + width + 1
-        resolved = list(zip(*(reversed(n) for n in nums)))
-        resolved = [int("".join(n)) for n in resolved]
-        if op == "*":
-            total += prod(int(n) for n in resolved)
-        elif op == "+":
-            total += sum(int(n) for n in resolved)
+        nums = [line[start : next_col - 1] for line in lines]
+        nums = list(zip(*(reversed(n) for n in nums)))
+        nums = [int("".join(n)) for n in nums]
+        total += OPERATORS[op](int(n) for n in nums)
     return total
 
 
