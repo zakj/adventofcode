8ed2b2720ff0c98f75ded051d1d23963c949c1bf
Zak Johnson <me@zakj.net>
2025-12-05 21:47:36 -0800

    25d6 more cleanup

--- py/2025/day06.py
+++ py/2025/day06.py
@@ -1,45 +1,38 @@
-import re
 from itertools import pairwise
 from math import prod
 
 from aoc import main
 
+type Problem = tuple[str, list[str]]  # operator, list of padded columns
 OPERATORS = {"+": sum, "*": prod}
 
 
-def part1(input: str):
-    lines = [re.split(r"\s+", line.strip()) for line in input.splitlines()]
-    total = 0
-    for x in range(len(lines[0])):
-        problem = []
-        for y in range(len(lines)):
-            problem.append(lines[y][x])
-        op = problem.pop()
-        total += OPERATORS[op](int(n) for n in problem)
-    return total
+def parse_problems(input: str) -> list[Problem]:
+    *lines, operators = input.splitlines()
+    cols = [i for i, c in enumerate(operators) if c != " "] + [len(operators) + 1]
+    return [
+        (operators[start], [line[start : next_col - 1] for line in lines])
+        for start, next_col in pairwise(cols)
+    ]
 
 
-def part2(input: str):
-    lines = input.splitlines()
-    operators = lines.pop()
-    cols = []
-    for i, c in enumerate(operators):
-        if c != " ":
-            cols.append(i)
-    cols.append(len(operators) + 1)
+def cephalopod_math(input: str) -> int:
+    return sum(
+        OPERATORS[op](int(n) for n in nums) for op, nums in parse_problems(input)
+    )
 
-    total = 0
-    for start, next_col in pairwise(cols):
-        op = operators[start]
-        nums = [line[start : next_col - 1] for line in lines]
-        nums = list(zip(*(reversed(n) for n in nums)))
-        nums = [int("".join(n)) for n in nums]
-        total += OPERATORS[op](int(n) for n in nums)
-    return total
+
+def transpose_columns(nums: list[str]) -> list[int]:
+    return [int("".join(n)) for n in zip(*(reversed(n) for n in nums))]
+
+
+def cephalopod_math_rtl(input: str) -> int:
+    problems = parse_problems(input)
+    return sum(OPERATORS[op](transpose_columns(nums)) for op, nums in problems)
 
 
 if __name__ == "__main__":
     main(
-        part1,
-        part2,
+        cephalopod_math,
+        cephalopod_math_rtl,
     )
