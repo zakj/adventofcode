16c90f1aa11930c069e7779fd04b9c68a781aa85
Zak Johnson <me@zakj.net>
2025-12-10 13:58:02 -0800

    25d10 p2 fiddling, still broken

--- py/2025/day10.py
+++ py/2025/day10.py
@@ -1,3 +1,4 @@
+from itertools import combinations_with_replacement
 import re
 
 from aoc import main, progress
@@ -44,37 +45,75 @@ def part1(input: str):
     return total
 
 
-class OnlineMachine[Node: set[int]](DiGraph):
-    def __init__(self, buttons: list[list[int]], max_joltages: list[int]) -> None:
-        self.buttons = buttons
-        self.max_joltages = max_joltages
-
-    def __getitem__(self, state: Node) -> set[Node]:
-        neighbors = set()
-        for button in self.buttons:
-            neighbor = list(state)
-            for idx in button:
-                neighbor[idx] += 1
-            over = False
-            for i, v in enumerate(neighbor):
-                if v > self.max_joltages[i]:
-                    over = True
-            if over:
-                continue
-            # print(f"{state=} {button=} {neighbor=}")
-            neighbors.add(tuple(neighbor))
-        return neighbors
-
-
-# XXX this won't work
 def part2(input: str):
     machines = parse(input)
+    machines = parse(
+        "[...###] (1,2,4) (2,4,5) (4,5) (0,5) (0,1,2,3,4) (0,1,5) (0,3) (3,5) {55,40,39,33,42,56}"
+    )
+
+    # [...###] (1,2,4) (2,4,5) (4,5) (0,5) (0,1,2,3,4) (0,1,5) (0,3) (3,5) {55,40,39,33,42,56}
+    #
+    # (0,5) (0,1,2,3,4) (0,1,5) (0,3)
+    # must be pressed a combined total of 55 times
+    # (1,2,4) (0,1,2,3,4) (0,1,5) -> 40
+    # (1,2,4) (2,4,5) (0,1,2,3,4) -> 39
+    # (0,1,2,3,4) (0,3) (3,5) -> 33
+    # (1,2,4) (2,4,5) (4,5) (0,1,2,3,4) -> 42
+    # (2,4,5) (4,5) (0,5) (0,1,5) (3,5) -> 56
+    #
+    # n = total presses
+    # k = button count
+    # comb(n + k - 1, k - 1) is total combinations
+    # too many to iterate
+    # but each permutation reduces the search space for the other buttons, so maybe we can?
+
+    # what if instead I pressed the buttons that affect the most indexes first?
+    # could a simple DFS work that way?
+    # for biggest button: max presses on that button is the smallest index it affects
+    # I could get the max presses per button
+    # and the min presses per set of buttons
+    # what would that look like?
+    #
+    # (0,5) (0,1,2,3,4) (0,1,5) (0,3) -> 55 01234 39 -> 16
+    # (1,2,4) (0,1,2,3,4) (0,1,5) -> 40 01234 39 -> 1
+    # (1,2,4) (2,4,5) (0,1,2,3,4) -> 39 01234 39 -> 0
+    # (0,1,2,3,4) (0,3) (3,5) -> 33
+    # (1,2,4) (2,4,5) (4,5) (0,1,2,3,4) -> 42 01234 39 -> 3
+    # (2,4,5) (4,5) (0,5) (0,1,5) (3,5) -> 56
+    #
+    # (01234) 39
+    # then we're stuck, 245 and 015
+
     total = 0
     for _lights, buttons, joltages in progress(machines):
+        buttons_affecting_index = {
+            i: [b for b in buttons if i in b] for i in range(len(joltages))
+        }
+
+        # n = total presses to distribute
+        # [(a, b, n - a - b) for a in range(n + 1) for b in range(n + 1 - a)]
+        # but this doesn't expand well to dynamic number of buttons :/
+
+        state = [0] * len(joltages)
+        for i in range(len(joltages)):
+            for presses in combinations_with_replacement(
+                buttons_affecting_index[i], joltages[i] - state[i]
+            ):
+                nstate = list(state)
+                for button in presses:
+                    for idx in button:
+                        nstate[idx] += 1
+
+        # for each combination of presses to get the first index
+        # reduce the target joltages as needed
+        # then recompute what's left
+
+        # index_with_lowest_presses = joltages.index(min(joltages))
+        # print(list(combinations_with_replacement(cur_buttons, lowest_presses))[100])
         # print("--", buttons, joltages)
-        machine = OnlineMachine(buttons, joltages)
-        presses = shortest_path_length(machine, (0,) * len(joltages), tuple(joltages))
-        total += presses
+        # machine = OnlineMachine(buttons, joltages)
+        # presses = shortest_path_length(machine, (0,) * len(joltages), tuple(joltages))
+        # total += presses
     return total
 
 
